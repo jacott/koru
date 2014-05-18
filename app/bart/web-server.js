f0b7ea1b0ed34cecf5e60f5afca14fb4ba8ca884
Geoff Jacobsen <geoffjacobsen@gmail.com>
2014-05-18 16:44:10 +1200

    Remove express

--- app/bart/web-server.js
+++ app/bart/web-server.js
@@ -1,6 +1,5 @@
 var Path = require('path');
 var http = require('http');
-var express = require('express');
 var send = require('send');
 var parseurl = require('parseurl');
 
@@ -12,31 +11,43 @@ define(function (require, exports, module) {
 
   var __dirname = Path.dirname(module.uri);
 
-  var app = express();
-
   var root = Path.resolve(__dirname + '/..');
 
-  app.use(function(req, res, next) {
+  var server = http.createServer(function (req, res) {
     core.Fiber(function () {
       var path = parseurl(req).pathname;
 
       var m = /^(.*\.build\/.*\.([^.]+))\.js$/.exec(path);
-      if (! (m && compileTemplate(req, res, next, m[2], root+m[1])))
-        next();
+      if (! (m && compileTemplate(req, res, m[2], root+m[1])))
+        send(req, path, {root: root})
+        .on('error', sendDefault)
+        .on('directory', sendDefault)
+        .pipe(res);
     }).run();
+
+    function sendDefault(err) {
+      send(req, '/demo-index.html', {root: root})
+        .on('error', error)
+        .pipe(res);
+    }
+
+    function error(err) {
+      if (! err || 404 === err.status) {
+        res.statusCode = 404;
+        res.end('NOT FOUND');
+      } else {
+        res.statusCode = 500;
+        res.end('Internal server error!');
+      }
+    }
   });
-
-  app.use(express.static(root));
-
-  var server = http.createServer(app);
 
   server.listen(3000);
 
-  exports.app = app;
   exports.server = server;
   exports.compilers = {};
 
-  function compileTemplate(req, res, next, type, path) {
+  function compileTemplate(req, res, type, path) {
     var compiler = exports.compilers[type];
     if (! compiler) return;
 
