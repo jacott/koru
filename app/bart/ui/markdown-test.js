26c9c06e3e67cfd3aa8d333f75b3342a89d586f3
Geoff Jacobsen <geoffjacobsen@gmail.com>
2014-05-22 12:11:48 +1200

    Add markdown

---
+++ app/bart/ui/markdown-test.js
@@ -0,0 +1,118 @@
+isClient && define(function (require, exports, module) {
+  var test, v;
+  var bt = require('bart/test');
+  var Markdown = require('./markdown');
+  var Dom = require('../dom');
+
+  bt.testCase(module, {
+    setUp: function () {
+      test = this;
+      v = {};
+    },
+
+    tearDown: function () {
+      v = null;
+    },
+
+    "fromHtml": {
+      setUp: function () {
+        v.c = function (html) {
+          return Markdown.fromHtml(Dom.html(html));
+        };
+      },
+
+      "test null": function () {
+        assert.same(Markdown.fromHtml(null), '');
+      },
+
+      "test href": function () {
+        assert.same(v.c('<div><a href="http://obeya.co">Obeya <b>Limited</b></a> link</div>'),
+                    '[Obeya **Limited**](http://obeya.co) link');
+
+        assert.same(v.c('<div><a href="http://obeya.co">Obeya_Limited_link</a> click it</div>'),
+                    '[Obeya_Limited_link](http://obeya.co) click it');
+
+        assert.same(v.c('<div><a href="http://obeya.co">Obeya **Limited**</a> link</div>'),
+                    '[Obeya \\**Limited**](http://obeya.co) link');
+      },
+
+      "test simple nesting": function () {
+        assert.same(v.c('<div><b>Brave <i>new</i> World</b></div>'),
+                    '**Brave _new_ World**');
+
+      },
+
+      "test complex": function () {
+        assert.same(v.c("<div><b>So <i>m</i> e</b> Text<div><br></div><div>As <i>html</i>  Test</div>" +
+                        "<div>ing with<br></div><div><br></div><div> spaces</div></div>"),
+                    '**So _m_ e** Text\n\nAs *html*  Test\ning with\n\n spaces');
+      },
+
+      "test buttons": function () {
+        assert.same(v.c('<div>Hello <span data-a="j2">Josiah&lt;JG&gt;</span></div>'), 'Hello @[Josiah<JG>](j2)');
+        assert.same(v.c('<div>Hello <span data-h="s1">Foo <b>bar</b></span></div>'), 'Hello #[Foo **bar**](s1)');
+      },
+    },
+
+    "toHtml": {
+      setUp: function () {
+        v.c = function (text) {
+          var elm = document.createElement('div');
+          elm.appendChild(Markdown.toHtml(text));
+          return elm.innerHTML;
+        };
+      },
+
+      "test links": function () {
+        assert.same(v.c('Hello @[Josiah<JG>](j2)'), 'Hello <span data-a="j2" contenteditable="false">Josiah&lt;JG&gt;</span>');
+        assert.same(v.c('#[Foo **bar**](s1)'), '<span data-h="s1" contenteditable="false">Foo <b>bar</b></span>');
+      },
+
+
+      "test wrapper": function () {
+        assert.dom(Markdown.toHtml("hello _world_", 'span'), function () {
+          assert.same(this.tagName, 'SPAN');
+          assert.dom('i', "world");
+        });
+      },
+
+      "test simple italic": function () {
+        assert.same(v.c('*italic* text'), "<i>italic</i> text");
+      },
+
+      "test simple bold": function () {
+        assert.same(v.c('**bold** text'), "<b>bold</b> text");
+      },
+
+      "test nested italic in bold": function () {
+        assert.same(v.c('**bold _italic_ ** text'), "<b>bold <i>italic</i> </b> text");
+        assert.same(v.c('__bold *italic* __ text'), "<b>bold <i>italic</i> </b> text");
+      },
+
+      "test nested bold in italic": function () {
+        assert.same(v.c('*italic __bold__ * text'), "<i>italic <b>bold</b> </i> text");
+      },
+
+      "test hyperlink": function () {
+        assert.same(v.c('[l1](/l1) text [O][b\n]eya](http://obeya.co)[link2](/a)'),
+                    '<a href="/l1">l1</a> text [O]<a href="http://obeya.co">b<br>]eya</a><a href="/a">link2</a>');
+      },
+
+      "test underscores in link text": function () {
+        assert.same(v.c('[Obeya_Limited_link](http://obeya.co) click it'),
+                    '<a href="http://obeya.co">Obeya_Limited_link</a> click it');
+
+        assert.same(v.c('[Obeya **Limited**](http://obeya.co) link'),
+                    '<a href="http://obeya.co">Obeya <b>Limited</b></a> link');
+
+        assert.same(v.c('[Obeya \\**Limited**](http://obeya.co) link'),
+                    '<a href="http://obeya.co">Obeya **Limited**</a> link');
+      },
+
+      "test complex": function () {
+        assert.same(v.c('**So _m_ e** Text\n\nAs *html*  Test\ning with\n\n spaces'),
+                    "<b>So <i>m</i> e</b> Text<div><br></div><div>As <i>html</i>  Test</div><div>ing with</div><div><br></div><div> spaces</div>");
+      },
+    },
+  });
+});
--- app/bart/ui/markdown.js
+++ app/bart/ui/markdown.js
@@ -1,92 +1,109 @@
-var output, needspace;
+define(function(require, exports, module) {
+  var util = require('../util');
 
-App.extend(App.Markdown, {
-  fromHtml: function (html) {
-    output = []; needspace = false;
-    html && outputChildNodes(html, '*');
-    var result = output.join('');
-    output = null;
-    return result;
-  },
+  var output, needspace, Markdown;
 
-  toHtml: function (md, wrapper) {
-    md = md || '';
-    var hypherlinks = App.Markdown.findHyperLinks(md);
-    var mdlen = md.length;
-    var index = 0;
-    var lookfor = [];
-    var frag = wrapper ? document.createElement(wrapper) : document.createDocumentFragment();
-    var elm = frag;
-    var token, mention;
-    var hlidx =  0;
-    var hlStart = hypherlinks.length ? hypherlinks[0][0] : mdlen;
-    var hlEnd = null;
-    while (index < mdlen) {
+  return Markdown = {
+    fromHtml: function (html) {
+      output = []; needspace = false;
+      html && outputChildNodes(html, '*');
+      var result = output.join('');
+      output = null;
+      return result;
+    },
 
-      // End of <a> textContent
-      if (hlEnd === index) {
-        var href = hypherlinks[hlidx][2];
-        if (mention) {
-          elm.setAttribute('data-'+ (mention === '@' ? 'a' : 'h'), href);
-          elm.setAttribute('contenteditable', 'false');
-        } else {
-          elm.setAttribute('href', href);
-        }
-        index += href.length + 3;
-        if (index === mdlen) break;
-        elm = elm.parentNode;
-        hlEnd = null;
-        mention = null;
-        hlStart = ++hlidx < hypherlinks.length ? hypherlinks[hlidx][0] : mdlen;
-      }
+    toHtml: function (md, wrapper) {
+      md = md || '';
+      var hypherlinks = Markdown.findHyperLinks(md);
+      var mdlen = md.length;
+      var index = 0;
+      var lookfor = [];
+      var frag = wrapper ? document.createElement(wrapper) : document.createDocumentFragment();
+      var elm = frag;
+      var token, mention;
+      var hlidx =  0;
+      var hlStart = hypherlinks.length ? hypherlinks[0][0] : mdlen;
+      var hlEnd = null;
+      while (index < mdlen) {
 
-      // Start of <a> textContent
-      if (hlStart === index) {
-        var link = document.createElement(mention ? 'span' : 'a');
-        ++index;
-        hlEnd = hlStart + hypherlinks[hlidx][1].length + 1;
-        elm.appendChild(link);
-        elm = link;
-      }
-
-
-      switch(md[index]) {
-        // <b> and <i>
-      case '*': case '_':
-        token = md[index];
-        if (md[++index] === token) {
-          ++index;
-          token = token+token;
-        }
-
-        if (lookfor.length && lookfor[lookfor.length - 1] == token) {
-          --lookfor.length;
+        // End of <a> textContent
+        if (hlEnd === index) {
+          var href = hypherlinks[hlidx][2];
+          if (mention) {
+            elm.setAttribute('data-'+ (mention === '@' ? 'a' : 'h'), href);
+            elm.setAttribute('contenteditable', 'false');
+          } else {
+            elm.setAttribute('href', href);
+          }
+          index += href.length + 3;
+          if (index === mdlen) break;
           elm = elm.parentNode;
-        } else {
-          var i = document.createElement(token.length === 1 ? 'i' : 'b');
-          elm.appendChild(i);
-          elm = i;
-          lookfor.push(token);
+          hlEnd = null;
+          mention = null;
+          hlStart = ++hlidx < hypherlinks.length ? hypherlinks[hlidx][0] : mdlen;
         }
-        break;
 
-        // <br> and <p>
-      case '\n':
-        if (md[++index] === "\n") {
+        // Start of <a> textContent
+        if (hlStart === index) {
+          var link = document.createElement(mention ? 'span' : 'a');
           ++index;
-          var p = document.createElement('p');
-          (elm.parentNode || elm).appendChild(p);
-          elm = p;
-        } else {
-          var br =  document.createElement('br');
-          elm.appendChild(br);
+          hlEnd = hlStart + hypherlinks[hlidx][1].length + 1;
+          elm.appendChild(link);
+          elm = link;
         }
-        break;
 
-        // textContent
-      default:
+        switch(md[index]) {
+        case '\\':
+          ++index;
+          textContent();
+          break;
+          // <b> and <i>
+        case '*': case '_':
+          if ((index !== 0 && md[index - 1].match(/[A-Za-z0-9*_]/) && (lookfor.length === 0 || (md[index + 1]||'').match(/[A-Za-z0-9]/)))) {
+            textContent();
+            break;
+          }
+          token = md[index];
+          if (md[++index] === token) {
+            ++index;
+            token = token+token;
+          }
+
+          if (lookfor.length && lookfor[lookfor.length - 1] == token) {
+            --lookfor.length;
+            elm = elm.parentNode;
+          } else {
+            var i = document.createElement(token.length === 1 ? 'i' : 'b');
+            elm.appendChild(i);
+            elm = i;
+            lookfor.push(token);
+          }
+          break;
+
+          // <br> and <div>
+        case '\n':
+          ++index;
+          if (elm.nodeType === document.DOCUMENT_FRAGMENT_NODE || elm.tagName === 'DIV') {
+            var div =  document.createElement('div');
+            var cur = (elm.parentNode || elm);
+            if (! elm.firstChild)
+              elm.appendChild(document.createElement('br'));
+            cur.appendChild(div);
+            elm = div;
+          } else {
+            elm.appendChild(document.createElement('br'));
+          }
+          break;
+
+        default:
+          textContent();
+        }
+      }
+      return frag;
+
+      function textContent() {
         var len = (hlEnd || hlStart);
-        for(var i = index; i < len && ! md[i].match(/[*_\n]/); ++i) {}
+        for(var i = index + 1; i < len && ! md[i].match(/[\\*_\n]/); ++i) {}
 
         var atHash = false;
 
@@ -96,100 +113,138 @@ App.extend(App.Markdown, {
         }
 
         if (i > index) {
-          var tn = document.createTextNode(md.slice(index, atHash ? i-1 : i));
+          if (elm.lastChild && elm.lastChild.nodeType === document.TEXT_NODE) {
+            elm.lastChild.textContent += md.slice(index, atHash ? i-1 : i);
+          } else {
+            var tn = document.createTextNode(md.slice(index, atHash ? i-1 : i));
+            elm.appendChild(tn);
+          }
           index = i;
-          elm.appendChild(tn);
         }
       }
-    }
-    return frag;
-  },
-});
+    },
 
-function fromHtml(html, flag) {
-  switch(html.nodeType) {
-  case document.TEXT_NODE:
-    spaceIfNeeded(html.textContent);
-    output.push(html.textContent.replace(/ /g, ' '));
-    break;
-  case document.ELEMENT_NODE:
-    switch(html.tagName) {
+    getMentionIds: function (md) {
+      var hypherlinks = this.findHyperLinks(md);
 
-    case 'B': case 'STRONG':
-      stressText(html, flag + flag);
-      break;
-
-    case 'I': case 'EM':
-      stressText(html, flag);
-      break;
-
-    case 'SPAN':
-      var id = html.getAttribute('data-a');
-      if (id) {
-        output.push('@[');
-      } else {
-        id = html.getAttribute('data-h');
-        id &&  output.push('#[');
+      for(var i = 0; i < hypherlinks.length; ++i) {
+        hypherlinks[i] = hypherlinks[i][2];
       }
-      if (id) {
+
+      return hypherlinks;
+    },
+
+    findHyperLinks: function(md) {
+      var m, re = /\[([\s\S]*?)\]\(([^)]*)\)/g;
+      var m2, re2 = /[\[\]]/g;
+      var result = [];
+      while ((m = re.exec(md)) !== null) {
+        re2.lastIndex = 0;
+        var nest = 1;
+        var lstart = m.index;
+        var mi = 0;
+        while ((m2 = re2.exec(m[1])) !== null) {
+          if (m2[0] === ']') nest > 0 && --nest;
+          else if (++nest === 1) {
+            mi = re2.lastIndex;
+            lstart += mi;
+          }
+        }
+        result.push([lstart, mi ? m[1].slice(mi) : m[1], m[2]]);
+      }
+      return result;
+    },
+  };
+
+  function fromHtml(html, flag) {
+    switch(html.nodeType) {
+    case document.TEXT_NODE:
+      spaceIfNeeded(html.textContent);
+      output.push(html.textContent.replace(/ /g, ' ').replace(/([^\w*])([\\_*[])/, '$1\\$2'));
+      break;
+    case document.ELEMENT_NODE:
+      switch(html.tagName) {
+
+      case 'B': case 'STRONG':
+        stressText(html, flag + flag);
+        break;
+
+      case 'I': case 'EM':
+        stressText(html, flag);
+        break;
+
+      case 'SPAN':
+        var id = html.getAttribute('data-a');
+        if (id) {
+          output.push('@[');
+        } else {
+          id = html.getAttribute('data-h');
+          id &&  output.push('#[');
+        }
+        if (id) {
+          outputChildNodes(html, flag);
+          output.push('](' + id + ')');
+          break;
+        }
         outputChildNodes(html, flag);
-        output.push('](' + id + ')');
+        break;
+
+      case 'BR':
+        if (! (html.parentNode && html.parentNode.lastChild === html))
+          output.push('\n');
+        break;
+
+      case 'A':
+        output.push('[');
+        outputChildNodes(html, flag);
+        output.push('](' + html.getAttribute('href') + ')');
+        break;
+
+      case 'DIV':
+        output.push('\n');
+        if (html.firstChild && html.firstChild.tagName === 'BR' && html.firstChild === html.lastChild) {
+        } else {
+          outputChildNodes(html, flag);
+        }
+        break;
+      case 'P':
+        output.push('\n\n');
+        outputChildNodes(html, flag);
+        break;
+      default:
+        outputChildNodes(html, flag);
         break;
       }
-      outputChildNodes(html, flag);
-      break;
-
-    case 'BR':
-      output.push('\n');
-      break;
-
-    case 'A':
-      output.push('[');
-      outputChildNodes(html, flag);
-      output.push('](' + html.getAttribute('href') + ')');
-      break;
-
-    case 'DIV':
-      output.push('\n');
-      outputChildNodes(html, flag);
-      break;
-    case 'P':
-      output.push('\n\n');
-      outputChildNodes(html, flag);
-      break;
-    default:
-      outputChildNodes(html, flag);
-      break;
     }
   }
-}
 
-function spaceIfNeeded(text) {
-  needspace && text.match(/^\S/) && output.push(' ');
-  needspace = false;
-}
-
-function stressText(html, flags) {
-  if (output.length && output[output.length -1].match(/\S$/))
-    output.push(' ');
-
-  needspace = false;
-  var flag = flags[0];
-  output.push(flags);
-  outputChildNodes(html, flipFlag(flag));
-  output.push(flags);
-  needspace = true;
-}
-
-function outputChildNodes(html, flag) {
-  var children = html.childNodes;
-
-  for(var i = 0; i < children.length; ++i) {
-    var row = children[i];
-    fromHtml(row, flag);
+  function spaceIfNeeded(text) {
+    needspace && text.match(/^\S/) && output.push(' ');
+    needspace = false;
   }
-}
 
-function flipFlag(flag) {
-  return flag === '*' ? '_' : '*';
-}
+  function stressText(html, flags) {
+    if (output.length && output[output.length -1].match(/\S$/))
+      output.push(' ');
+
+    needspace = false;
+    var flag = flags[0];
+    output.push(flags);
+    outputChildNodes(html, flipFlag(flag));
+    output.push(flags);
+    needspace = true;
+  }
+
+  function outputChildNodes(html, flag) {
+    var children = html.childNodes;
+
+    for(var i = 0; i < children.length; ++i) {
+      var row = children[i];
+      fromHtml(row, flag);
+    }
+  }
+
+  function flipFlag(flag) {
+    return flag === '*' ? '_' : '*';
+  }
+});
