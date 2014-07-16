e9b28c2fe2fdda11ef49c115fd6fca06edfeb0c2
Geoff Jacobsen <geoffjacobsen@gmail.com>
2014-07-16 21:45:57 +1200

    Allow publish with module in place of name
    with module will auto unpublish onunload

--- app/koru/session/publish-base.js
+++ app/koru/session/publish-base.js
@@ -8,7 +8,20 @@ define(function(require, exports, module) {
 
   var pubs = {};
 
-  function publish(name, func) {
+  function publish(module, name, func) {
+    if (typeof module === 'string') {
+      func = name;
+      name = module;
+    } else {
+      koru.onunload(module, function () {
+        publish._destroy(name);
+      });
+      if (typeof name !== 'string') {
+        func = name;
+        name = util.capitalize(util.camelize(module.id.replace(/^.*\//, '').replace(/-(server|client)$/, '')));
+      }
+    }
+
     if (name in pubs) throw new Error("Already published: " + name);
     pubs[name] = func;
   }
