d64cc23c57ae4fe937d8d573c4e83316eb88a45b
Geoff Jacobsen <geoffjacobsen@gmail.com>
2014-08-15 10:06:16 +1200

    Improve copyright info

--- app/koru/srp/big-integer.js
+++ app/koru/srp/big-integer.js
@@ -1,7 +1,8 @@
 define(function() {
-  /// BEGIN jsbn.js
-
   /*
+   * jsbn.js - Basic JavaScript BN library - subset useful for RSA encryption.
+   * jsbn2.js - Extended JavaScript BN functions, required for RSA private ops.
+   *
    * Copyright (c) 2003-2005  Tom Wu
    * All Rights Reserved.
    *
@@ -31,8 +32,6 @@ define(function() {
    * All redistributions must retain an intact copy of this copyright notice
    * and disclaimer.
    */
-
-  // Basic JavaScript BN library - subset useful for RSA encryption.
 
   // Bits per digit
   var dbits;
@@ -593,41 +592,6 @@ define(function() {
   BigInteger.ZERO = nbv(0);
   BigInteger.ONE = nbv(1);
 
-
-  /// BEGIN jsbn2.js
-
-  /*
-   * Copyright (c) 2003-2005  Tom Wu
-   * All Rights Reserved.
-   *
-   * Permission is hereby granted, free of charge, to any person obtaining
-   * a copy of this software and associated documentation files (the
-   * "Software"), to deal in the Software without restriction, including
-   * without limitation the rights to use, copy, modify, merge, publish,
-   * distribute, sublicense, and/or sell copies of the Software, and to
-   * permit persons to whom the Software is furnished to do so, subject to
-   * the following conditions:
-   *
-   * The above copyright notice and this permission notice shall be
-   * included in all copies or substantial portions of the Software.
-   *
-   * THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
-   * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
-   * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
-   *
-   * IN NO EVENT SHALL TOM WU BE LIABLE FOR ANY SPECIAL, INCIDENTAL,
-   * INDIRECT OR CONSEQUENTIAL DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER
-   * RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER OR NOT ADVISED OF
-   * THE POSSIBILITY OF DAMAGE, AND ON ANY THEORY OF LIABILITY, ARISING OUT
-   * OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-   *
-   * In addition, the following condition applies:
-   *
-   * All redistributions must retain an intact copy of this copyright notice
-   * and disclaimer.
-   */
-
-  // Extended JavaScript BN functions, required for RSA private ops.
 
   // (public)
   function bnClone() { var r = nbi(); this.copyTo(r); return r; }
@@ -1269,6 +1233,5 @@ define(function() {
   // long longValue()
   // static BigInteger valueOf(long val)
 
-  /// METEOR WRAPPER
   return BigInteger;
 });
