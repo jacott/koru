1821f9975a267394862c8b065f1a21c3d848dc62
Geoff Jacobsen <geoffjacobsen@gmail.com>
2016-02-09 13:27:09 +1300

    Add ui/resize-event

---
+++ app/koru/ui/resize-event-test.js
@@ -0,0 +1,64 @@
+isClient && define(function (require, exports, module) {
+  var test, v;
+  var TH = require('./test-helper');
+  var sut = require('./resize-event');
+  var Dom = require('koru/dom');
+
+  TH.testCase(module, {
+    setUp: function () {
+      test = this;
+      v = {};
+      v.raf = test.stub(window, 'requestAnimationFrame');
+      v.html = Dom.h({div: 'foo', $style: "width:100px;height:150px"});
+      document.body.appendChild(v.html);
+      v.resizer = sut.onWidthResize(v.html, v.resized = test.stub());
+    },
+
+    tearDown: function () {
+      Dom.remove(v.html);
+      v = null;
+    },
+
+    "test resize width": function () {
+      assert.calledWith(v.raf, TH.match.func);
+      v.raf.yield();
+      v.raf.reset();
+      refute.called(v.resized);
+
+      assert.dom('.resize-detector', function () {
+        v.html.style.width = '110px';
+        assert.same(this.getAttribute('style'), 'position:relative;width:100%;height:0;pointer-events:none;visibility:hidden;');
+        assert.same(this.firstChild.getAttribute('style'), 'position:absolute;left:0;width:100%;height:20px;overflow-x:scroll');
+        assert.same(this.lastChild.getAttribute('style'), 'position:absolute;left:0;width:100%;height:20px;overflow-x:scroll');
+        assert.same(this.firstChild.firstChild.getAttribute('style'), 'width:200%;height:100%');
+        assert.same(this.lastChild.firstChild.getAttribute('style'), 'width:200vw;height:100%');
+
+        TH.trigger(this.firstChild, 'scroll');
+        TH.trigger(this.firstChild, 'scroll');
+        assert.called(v.raf);
+        v.raf.yield();
+        assert.calledOnceWith(v.resized, v.html, 110);
+        v.resized.reset(); v.raf.reset();
+        assert.same(this.firstChild.scrollLeft, 110);
+        assert.same(this.lastChild.scrollLeft, window.innerWidth*2 - 110);
+
+        v.html.style.width = '90px';
+        TH.trigger(this.firstChild, 'scroll');
+        v.raf.yield();
+        assert.calledOnceWith(v.resized, v.html, 90);
+        assert.same(this.firstChild.scrollLeft, 90);
+        assert.same(this.lastChild.scrollLeft, window.innerWidth*2 - 90);
+
+        v.resized.reset(); v.raf.reset();
+        TH.trigger(this.firstChild, 'scroll');
+        v.raf.yield();
+        refute.called(v.resized);
+
+        v.html.style.width = '80px';
+        this.firstChild.scrollLeft = 0;
+        v.resizer.reset();
+        assert.same(this.firstChild.scrollLeft, 80);
+      });
+    },
+  });
+});
---
+++ app/koru/ui/resize-event.js
@@ -0,0 +1,51 @@
+define(function(require, exports, module) {
+  var Dom = require('koru/dom');
+
+  var WIDTH_RESIZE_ELM = Dom.h({
+    div: [{
+      div: {div: '', $style: 'width:200%;height:100%'},
+      $style: 'position:absolute;left:0;width:100%;height:20px;overflow-x:scroll',
+    }, {
+      div: {div: '', $style: 'width:200vw;height:100%'},
+      $style: 'position:absolute;left:0;width:100%;height:20px;overflow-x:scroll',
+    }],
+    class: 'resize-detector',
+    $style: 'position:relative;width:100%;height:0;pointer-events:none;visibility:hidden;',
+  });
+
+  exports.onWidthResize = function (elm, callback) {
+    var resizer = WIDTH_RESIZE_ELM.cloneNode(true);
+    elm.appendChild(resizer);
+    var offsetWidth, waiting = true;
+    window.requestAnimationFrame(fire);
+
+    resizer.firstChild.addEventListener('scroll', scrolled);
+    resizer.lastChild.addEventListener('scroll', scrolled);
+
+    detach.reset = fire;
+
+    return detach;
+
+    function detach() {
+      resizer.firstChild.removeEventListener('scroll', scrolled);
+      resizer.lastChild.removeEventListener('scroll', scrolled);
+      Dom.remove(resizer);
+      waiting = false;
+    }
+
+    function fire() {
+      var nw = elm.offsetWidth;
+      if (waiting === callback && nw !== offsetWidth)
+        callback(elm, nw);
+      offsetWidth = nw;
+      resizer.firstChild.scrollLeft = resizer.lastChild.scrollLeft = 10000;
+      waiting = false;
+    }
+
+    function scrolled(event) {
+      if (waiting) return;
+      waiting = callback;
+      window.requestAnimationFrame(fire);
+    }
+  };
+});
