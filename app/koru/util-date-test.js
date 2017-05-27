e3dc8c1c572ba218fffb02e9beb2ec142adf7d54
Geoff Jacobsen <geoffjacobsen@gmail.com>
2017-05-27 20:52:34 +1200

    util-date: add compileFormat, format and relative

--- app/koru/util-date-test.js
+++ app/koru/util-date-test.js
@@ -2,18 +2,12 @@ define(function (require, exports, module) {
   const TH    = require('./test');
 
   const uDate = require('./util-date');
-  var test, v;
+
+  const MIN = 60*1000;
+  const HOUR = MIN*60;
+  const DAY = 24*HOUR;
 
   TH.testCase(module, {
-    setUp() {
-      test = this;
-      v = {};
-    },
-
-    tearDown() {
-      v = null;
-    },
-
     "test atHour"() {
       const date = uDate.atHour(Date.UTC(2014,4,5), 6);
       assert.same(date.toISOString(), '2014-05-05T06:00:00.000Z');
@@ -31,6 +25,40 @@ define(function (require, exports, module) {
 
       date = uDate.atDowHour(Date.UTC(2014,4,10), thu, 9);
       assert.same(date.toISOString(), '2014-05-15T09:00:00.000Z');
+    },
+
+    "test format"() {
+      const d = new Date(2017, 0, 4, 14, 3, 12);
+      const format = uDate.compileFormat('D MMM YYYY h:mma');
+      assert.same(format(d), '4 Jan 2017 2:03pm');
+      assert.same(format(12*HOUR + +d), '5 Jan 2017 2:03am');
+
+      assert.same( uDate.format(d, `DD'YY hh`), `04'17 02`);
+    },
+
+    "test from"() {
+      assert.same(uDate.relative(0), 'a few seconds');
+      assert.same(uDate.relative(44999), 'a few seconds');
+      assert.same(uDate.relative(45000), 'a minute');
+      assert.same(uDate.relative(89999), 'a minute');
+      assert.same(uDate.relative(90000), '2 minutes');
+      assert.same(uDate.relative(44.5*MIN-1), '44 minutes');
+      assert.same(uDate.relative(44.5*MIN), 'an hour');
+      assert.same(uDate.relative(90*MIN-1), 'an hour');
+      assert.same(uDate.relative(90*MIN), '2 hours');
+      assert.same(uDate.relative(22.5*HOUR-1), '22 hours');
+      assert.same(uDate.relative(22.5*HOUR), 'a day');
+      assert.same(uDate.relative(36*HOUR-1), 'a day');
+      assert.same(uDate.relative(36*HOUR), '2 days');
+      assert.same(uDate.relative(26.5*DAY-1), '26 days');
+      assert.same(uDate.relative(26.5*DAY), 'a month');
+      assert.same(uDate.relative(45*DAY), 'a month');
+      assert.same(uDate.relative(46*DAY), '2 months');
+      assert.same(uDate.relative(319*DAY), '10 months');
+      assert.same(uDate.relative(320*DAY), 'a year');
+      assert.same(uDate.relative(548*DAY-1), 'a year');
+      assert.same(uDate.relative(548*DAY), '2 years');
+      assert.same(uDate.relative(5000*DAY), '14 years');
     },
   });
 });
--- app/koru/util-date.js
+++ app/koru/util-date.js
@@ -1,9 +1,43 @@
 define(function(require, exports, module) {
-  const util = require('koru/util');
+  const MIN = 60*1000;
+  const HOUR = MIN*60;
+  const DAY = 24*HOUR;
+  const AVG_YEAR = 365.25*DAY;
+  const AVG_MONTH = AVG_YEAR/12;
 
-  const DAY = 24*60*60*1000;
+  const MONTH_NAMES = 'Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec'.split(' ');
 
-  return {
+  const SPLITTER = /(D{1,2}|M{3}|Y{2,4}|h{1,2}|m{1,2}|s{1,3}|a)/;
+
+  const tmpDate = new Date(0);
+
+  const TOKENS = {
+    D: ()=> ''+tmpDate.getDate(),
+    DD: ()=> {
+      const d = tmpDate.getDate();
+      return (d < 10 ? '0' : '')+d;
+    },
+    MMM: ()=> ''+MONTH_NAMES[tmpDate.getMonth()],
+    YYYY: ()=> ''+tmpDate.getFullYear(),
+    YY: ()=> (''+tmpDate.getYear()).slice(-2),
+    h: ()=> {
+      const hours = tmpDate.getHours();
+      return hours == 0 ? '12' : ''+(hours > 12 ? (hours - 12) : hours);
+    },
+    hh: ()=> {
+      const h = TOKENS.h();
+      return h.length == 1 ? '0'+h : h;
+    },
+    mm: ()=> {
+      const min = tmpDate.getMinutes();
+      return (min < 10 ? '0' : '')+min;
+    },
+    a: ()=> tmpDate.getHours() < 12 ? 'am' : 'pm',
+  };
+
+  const uDate = {
+    MIN, HOUR, DAY,
+
     atHour(date, hour) {
       var orig = +date;
       date = new Date(orig);
@@ -27,5 +61,47 @@ define(function(require, exports, module) {
 
       return new Date( (orig > date ? 7*DAY : 0) + date);
     },
+    format(d, format) {
+      return uDate.compileFormat(format)(d);
+    },
+    relative(delta) {
+      if (delta < 0) delta = -delta;
+      if (delta < 45000) return 'a few seconds';
+
+      if (delta < 90000) return 'a minute';
+      if (delta < 44.5*MIN) return `${Math.round(delta/MIN)} minutes`;
+
+      if (delta < 90*MIN) return 'an hour';
+      if (delta < 22.5*HOUR) return `${Math.round(delta/HOUR)} hours`;
+
+      if (delta < 36*HOUR) return 'a day';
+      if (delta < 26.5*DAY) return `${Math.round(delta/DAY)} days`;
+
+      if (delta < 46*DAY) return 'a month';
+      if (delta < 320*DAY) return `${Math.round(delta/AVG_MONTH)} months`;
+
+      if (delta < 548*DAY) return 'a year';
+      return `${Math.round(delta/AVG_YEAR)} years`;
+    },
+    compileFormat(format) {
+      const parts = format.split(SPLITTER);
+      const len = parts.length;
+      for(let i = 0; i < len; ++i) {
+        const token = TOKENS[parts[i]];
+        if (token !== undefined) parts[i] = token;
+      }
+
+      return date => {
+        tmpDate.setTime(+date);
+        let ans = '';
+        for(let i = 0; i < len; ++i) {
+          const part = parts[i];
+          ans+= typeof part === 'string' ? part : part();
+        }
+        return ans;
+      };
+    },
   };
+
+  return uDate;
 });
