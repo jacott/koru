e0d9697b127206a2fd793bf19b84675b2887945b
Geoff Jacobsen <geoffjacobsen@gmail.com>
2017-08-08 12:49:23 +1200

    associated-validator: ensure always sorted and unique

--- app/koru/model/validators/associated-validator-test.js
+++ app/koru/model/validators/associated-validator-test.js
@@ -21,7 +21,7 @@ define(function (require, exports, module) {
 
     "test filter"() {
       const foo_ids = ["xyz", "def", "abc"],
-            doc = {foo_ids: foo_ids};
+            doc = {foo_ids: foo_ids, attributes: {}};
 
       const forEach = test.stub(Query.prototype, 'forEach', function (func) {
         func({_id: "abc"});
@@ -45,7 +45,7 @@ define(function (require, exports, module) {
 
     "test empty filter"() {
       const foo_ids = ["abc", "def", "xyz"],
-            doc = {foo_ids: foo_ids};
+            doc = {foo_ids: foo_ids, attributes: {}};
 
       const forEach = test.intercept(Query.prototype, 'forEach');
 
@@ -68,7 +68,7 @@ define(function (require, exports, module) {
         assert.same(this.model, Model.Foo);
         return 0;
       });
-      const doc = {foo_ids: ["xyz"]};
+      const doc = {foo_ids: ["xyz"], attributes: {}};
       sut(doc,'foo_ids', true);
 
       assert(doc._errors);
@@ -76,15 +76,110 @@ define(function (require, exports, module) {
     },
 
     "test changes only"() {
-      const doc = {foo_ids: ["xyz"], changes: {}};
+      const doc = {
+        get foo_ids() {return this.changes.foo_ids},
+        changes: {},
+        attributes: {foo_ids: ["bef", "foo", "xyz"]},
+      };
 
       sut(doc,'foo_ids', {changesOnly: true});
 
       refute(doc._errors);
+
+      let count = 2;
+
+      test.intercept(Query.prototype, 'count', ()=>count);
+      test.intercept(Query.prototype, 'forEach', func=>{
+        func({_id: "abc"});
+        func({_id: "can"});
+      });
+
+      doc.changes.foo_ids = ["can", "def", "bef", "xyz", "abc"];
+
+      sut(doc,'foo_ids', {changesOnly: true, filter: true});
+
+      refute(doc._errors);
+      assert.equals(doc.changes.foo_ids, ["abc", "bef", "can", "xyz"]);
+
+      sut(doc,'foo_ids', {changesOnly: true});
+      refute(doc._errors);
+
+      doc.changes.foo_ids = ["can", "def", "bef", "xyz", "abc", 'new'];
+      sut(doc,'foo_ids', {changesOnly: true});
+
+      assert(doc._errors);
+
+      count = 1;
+      doc._errors = undefined;
+      doc.changes.foo_ids = ['M', 'E'];
+      doc.attributes.foo_ids = ['M'];
+
+      sut(doc,'foo_ids', {changesOnly: true});
+      refute(doc._errors);
+      assert.equals(doc.changes.foo_ids, ['E', 'M']);
+
+      count = 3;
+      doc._errors = undefined;
+      doc.changes.foo_ids = ['ra', 'rq', 'A', 'B', 'ra', 'A'];
+      doc.attributes.foo_ids = ['ra'];
+
+      sut(doc,'foo_ids', {changesOnly: true});
+      refute(doc._errors);
+      assert.equals(doc.changes.foo_ids, ['A', 'B', 'ra', 'rq']);
+    },
+
+    "duplicates are not allowed": {
+      setUp() {
+        /**
+         * Duplicates are not allowed. ids are in ascending order
+         **/
+      },
+
+      "test unfiltered"() {
+        test.intercept(Query.prototype, 'count', ()=>4);
+
+        const doc = {
+          attributes: {foo_ids: ["b", "d", "a", "c"]},
+          changes: {foo_ids: ["f", "a", "b", "e", "a", "e"]},
+
+          get foo_ids() {return this.changes.foo_ids},
+        };
+
+        sut(doc,'foo_ids', {});
+
+        assert.equals(doc.changes.foo_ids, ["a", "a", "b", "e", "e", "f"]);
+        assert.equals(doc._errors, {foo_ids: [['duplicates']]});
+      },
+
+      "test filtered"() {
+        test.intercept(Query.prototype, 'forEach', func=>{
+          func({_id: 'f'});
+          func({_id: 'e'});
+        });
+
+        const doc = {
+          attributes: {foo_ids: ["a", "c", "b", "d"]},
+          changes: {foo_ids: ["f", "a", "b", "e", "a", "e"]},
+
+          get foo_ids() {return this.changes.foo_ids},
+        };
+
+        sut(doc,'foo_ids', {changesOnly: true, filter: true});
+        refute(doc._errors);
+
+        // don't filter out old ids
+        assert.equals(doc.changes.foo_ids, ["a", "b", "e", "f"]);
+
+        sut(doc,'foo_ids', {filter: true});
+        refute(doc._errors);
+
+        // filter out old ids
+        assert.equals(doc.changes.foo_ids, ["e", "f"]);
+      },
     },
 
     "test wrong type"() {
-      const doc = {foo_ids: "abc"};
+      const doc = {foo_ids: "abc", attributes: {}};
       sut(doc,'foo_ids', true);
 
       assert(doc._errors);
@@ -92,7 +187,7 @@ define(function (require, exports, module) {
     },
 
     "test using scoped finder"() {
-      const doc = {foo_ids: v.foo_ids = ['x', 'y']};
+      const doc = {foo_ids: v.foo_ids = ['x', 'y'], attributes: {}};
 
       function fooFinder(values) {
         v.values = values;
@@ -111,7 +206,8 @@ define(function (require, exports, module) {
         fooFind(values) {
           v.values = values;
           return {count: test.stub().returns(2)};
-        }
+        },
+        attributes: {},
       };
 
       sut(doc,'foo_ids', true);
@@ -124,7 +220,7 @@ define(function (require, exports, module) {
       const bar_ids = ['x', 'y'];
 
       const count = test.stub(Query.prototype, 'count').returns(2);
-      const doc = {bar_ids: bar_ids};
+      const doc = {bar_ids: bar_ids, attributes: {}};
       const where = stubWhere();
 
       sut(doc,'bar_ids', {modelName: 'Foo'});
@@ -143,8 +239,11 @@ define(function (require, exports, module) {
 
     "test belongs_to"() {
       const count = test.stub(Query.prototype, 'count').returns(1);
-      const doc = {foo_id: "x", constructor: {
-        $fields: {foo_id: {type: 'belongs_to', model: Model.Foo}}}};
+      const doc = {
+        foo_id: "x", constructor: {
+          $fields: {foo_id: {type: 'belongs_to', model: Model.Foo}}},
+        attributes: {},
+      };
 
       const where = stubWhere();
 
@@ -161,7 +260,7 @@ define(function (require, exports, module) {
       const foo_ids = ['x', 'y'];
 
       const count = test.stub(Query.prototype, 'count').returns(2);
-      const doc = {foo_ids: foo_ids};
+      const doc = {foo_ids: foo_ids, attributes: {}};
 
       const where = stubWhere();
 
--- app/koru/model/validators/associated-validator.js
+++ app/koru/model/validators/associated-validator.js
@@ -3,10 +3,16 @@ define(function(require, exports, module) {
   const Model = require('../main');
   const Query = require('../query');
 
+  const toMap = list => {
+    const ans = {}, {length} = list;
+    for(let i = 0; i < length; ++i) ans[list[i]] = true;
+    return ans;
+  };
+
   return function (doc, field, options) {
     if (options.changesOnly && ! (field in doc.changes)) return;
     let value = doc[field];
-    if (! value) return;
+    if (value == null) return;
 
     const fieldOpts = doc.constructor.$fields && doc.constructor.$fields[field];
     const belongs_to = fieldOpts && fieldOpts.type === 'belongs_to';
@@ -16,6 +22,8 @@ define(function(require, exports, module) {
 
     if (belongs_to)
       value = [value];
+
+    if (value.length === 0) return;
 
     switch (typeof options) {
     case 'object':
@@ -42,15 +50,72 @@ define(function(require, exports, module) {
       doc[scopeName+'Find'] ||
       (values => new Query(Model[modelName]).where('_id', values));
 
-    if (filter) {
-      var query = finder.call(doc, value.slice()); // stop from being clobbered
+
+
+    const orig = doc.attributes[field];
+    if (options.changesOnly && orig != null && orig.length != 0 && value.length != 0) {
+      orig.sort();
+      value.sort();
+      const newIds = [];
+      let ki = 0;
+      const ol = orig.length, nl = value.length;
+      let ni = 0, nv = value[0];
+      for(let i = 0; i < ol; ++i) {
+        const ov = orig[i];
+        while (nv < ov && ++ni < nl) {
+          if(newIds.length == 0 || newIds[newIds.length-1] !== nv)
+            newIds.push(nv);
+          nv = value[ni];
+        }
+        if (nv === ov) {
+          while (nv == ov && ni < nl)
+            nv = value[++ni];
+          value[ki++] = ov;
+        }
+      }
+
+
+      for (let i = ni; i < nl; ++i) newIds.push(value[i]);
+
+      if (filter) {
+        finder.call(doc, newIds).fields('_id').forEach(assoc => {
+          value[ki++] = assoc._id;
+        });
+        value.length = ki;
+        value.sort();
+        if (belongs_to) doc[field] = value[0];
+      } else if (finder.call(doc, newIds).count() !== newIds.length) {
+        this.addError(doc,field,'not_found');
+      } else {
+        for(let i = 0; i < newIds.length; ++i) {
+           value[ki++] = newIds[i];
+        }
+        value.length = ki;
+        value.sort();
+      }
+    } else if (filter) {
+      const query = finder.call(doc, value.slice()); // stop from being clobbered
       value.length = 0;
 
       query.fields('_id').forEach(assoc => value.push(assoc._id));
       value.sort();
+
       if (belongs_to) doc[field] = value[0];
-    } else if (finder.call(doc, value).count() !== value.length) {
-      this.addError(doc,field,'not_found');
+    } else {
+      value.sort();
+      const vl = value.length;
+      let lv = value[0];
+      for(let i = 1; i < vl; ++i) {
+        const v = value[i];
+        if (lv == v) {
+          this.addError(doc,field, 'duplicates');
+          return;
+        }
+      }
+      if (finder.call(doc, value).count() !== value.length) {
+        this.addError(doc,field, 'not_found');
+      }
+
     }
   };
 });
