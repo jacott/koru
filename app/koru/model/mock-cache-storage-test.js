760f114a603933cfd00ff94db097eabb74cb20d0
Geoff Jacobsen <geoffjacobsen@gmail.com>
2017-12-17 10:18:49 +1300

    add mock-cache-storage

---
+++ app/koru/model/mock-cache-storage-test.js
@@ -0,0 +1,118 @@
+isClient && define(function (require, exports, module) {
+  const MockPromise     = require('koru/test/mock-promise');
+  const TH              = require('./test-helper');
+
+  const {stub, spy, onEnd, util} = TH;
+  const {Request} = window;
+
+  const sut  = require('./mock-cache-storage');
+
+  const poll = ()=>{MockPromise._poll()};
+
+  let v = null;
+  TH.testCase(module, {
+    setUp() {
+      v = {};
+      v.caches = new sut();
+      TH.stubProperty((isServer ? global : self), 'Promise', {value: MockPromise});
+    },
+
+    tearDown() {
+      v = null;
+    },
+
+    "test open"() {
+      const p = v.caches.open('foo');
+      assert(p instanceof MockPromise);
+      p.then(c => v.cache = c);
+
+      poll();
+
+      assert(v.cache);
+      assert.same(v.cache, v.caches._caches.foo);
+
+      v.caches.open('foo').then(c => v.same = c);
+      v.caches.open('bar').then(c => v.notsame = c);
+
+      poll();
+
+      assert.same(v.same, v.cache);
+      refute.same(v.notsame, v.cache);
+    },
+
+    "with cache": {
+      setUp() {
+        v.caches.open('foo').then(c => v.cache = c); poll();
+        v.req = new Request('/foo.js'); v.resp = {status: 200, myrep: 'foo'};
+        v.cache.put(v.req, v.resp); poll();
+      },
+
+      "test put, match"() {
+        const {cache} = v;
+
+        const req = new Request('/index.js'), resp = {status: 200, myrep: 'index'};
+
+        const putPromise = cache.put(req, resp);
+        assert.same(putPromise._state, 'resolved');
+
+        const matchPromise = cache.match(new Request('/index.js'));
+
+        assert.same(matchPromise._state, 'resolved');
+
+        matchPromise.then(r => v.ans = r);
+
+        poll();
+
+        assert.same(v.ans, resp);
+
+        v.caches.match(req).then(r => v.cachesMatch = r);
+
+        poll();
+
+        assert.same(v.cachesMatch, resp);
+      },
+
+      "test delete"() {
+        const {cache, req, resp} = v;
+
+        cache.delete(req).then(r => v.del = r);
+
+        cache.match(req).then(r => v.ans = r);
+        poll();
+
+        assert.same(v.ans, undefined);
+        assert.isTrue(v.del);
+
+        cache.delete(req).then(r => v.del = r);
+        poll();
+        assert.isFalse(v.del);
+      },
+
+      "test caches.keys"() {
+        const {caches, cache: foo} = v;
+        let bar;
+
+        caches.open('bar').then(c => bar = c); poll();
+
+        caches.keys().then(keyList => v.ans = keyList);
+
+        poll();
+
+        assert.equals(v.ans.sort(), ['bar', 'foo']);
+      },
+
+      "test caches.delete"() {
+        v.caches.delete('foo').then(r => v.del = r);
+        poll();
+        assert.isTrue(v.del);
+
+        assert.equals(v.caches._caches, {});
+
+        v.caches.delete('foo').then(r => v.del = r);
+        poll();
+        assert.isFalse(v.del);
+
+      },
+    },
+  });
+});
---
+++ app/koru/model/mock-cache-storage.js
@@ -0,0 +1,64 @@
+define(function(require, exports, module) {
+  const BTree           = require('koru/btree');
+  const util            = require('koru/util');
+
+  const cacheCompare = (a, b)=>{
+    if (a.url === b.url) return 0;
+    return a.url < b.url ? -1 : 1;
+  };
+
+
+  class MockCache {
+    constructor() {
+      this._btree = new BTree(cacheCompare);
+    }
+
+    put(req, response) {
+      const entry = {url: req.url, response};
+      this._btree.add(entry);
+      return Promise.resolve();
+    }
+
+    match(req) {
+      const v = this._btree.find({url: req.url});
+      return Promise.resolve(v && v.response);
+    }
+
+    delete(req) {
+      const v = this._btree.delete({url: req.url});
+      return Promise.resolve(!!v);
+    }
+  }
+
+  class MockCacheStorage {
+    constructor() {
+      this._caches = {};
+    }
+
+    open(name) {
+      return Promise.resolve(this._caches[name] || (this._caches[name] = new MockCache()));
+    }
+
+    match(req) {
+      let ans;
+      for (const name in this._caches) {
+        ans = this._caches[name].match(req);
+        if (ans !== undefined) break;
+      }
+      return Promise.resolve(ans);
+    }
+
+    keys() {
+      return Promise.resolve(Object.keys(this._caches));
+    }
+
+    delete(name) {
+      return Promise.resolve(
+        this._caches[name] === undefined ? false : (
+          delete this._caches[name], true
+        ));
+    }
+  }
+
+  return MockCacheStorage;
+});
