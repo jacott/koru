3105fbbc9abcd47c1713c1e1445d6a9cc9e1b28d
Geoff Jacobsen <geoffjacobsen@gmail.com>
2018-03-19 21:53:23 +1300

    message-queues: refactor to mq-factory

--- app/koru/model/mq-factory-test.js
+++ app/koru/model/mq-factory-test.js
@@ -8,45 +8,41 @@ isServer && define(function (require, exports, module) {
 
   const {stub, spy, onEnd, util, intercept} = TH;
 
-  const sut  = require('./message-queues');
+  const MQFactory = require('./mq-factory');
   let v = null;
 
+  let sut;
+
   TH.testCase(module, {
-    setUpOnce() {
+    setUp() {
+      api.module();
       v = {};
       v.defDb = Driver.defaultDb;
       TH.coreStartTransaction(v.defDb);
-    },
-
-    tearDownOnce() {
-      v.defDb.query(`drop table  if exists _message_queue;
-drop sequence if exists _message_queue__id_seq;
-`);
-      TH.coreRollbackTransaction(v.defDb);
-      v = null;
-    },
-
-    setUp() {
-      api.module();
-      TH.startTransaction(v.defDb);
+      sut = new MQFactory('_test_MQ');
     },
 
     tearDown() {
       sut.deregisterQueue('foo');
       sut.stopAll();
-      TH.rollbackTransaction(v.defDb);
+      v.defDb.query(`drop table  if exists "_test_MQ"; drop sequence if exists "_test_MQ__id_seq";`);
+      TH.coreRollbackTransaction(v.defDb);
+      v = sut = null;
     },
 
-    "test registerQueue"() {
+    "test global registerQueue"() {
       /**
        * Register an action with a queue
 
-       * @param [module] unregister if module is unloaded
+       * @param [module] unregister if module is unloaded. Not available if local is true
 
        * @param name the name of the queue
 
        * @param [retryInterval] number of ms to wait before retrying a failed action.
        Defaults to 60000. A value of -1 means don't retry
+
+       * @param [local] defaults to false. If true register queue just the current database;
+       * otherwise register the queue for all current and future databases.
 
        * @param action the action to run when a queued message is ready
        **/
@@ -67,13 +63,15 @@ drop sequence if exists _message_queue__id_seq;
     },
 
     "with multi-db": {
-      setUpOnce() {
+      setUp() {
+        dbBroker.db = v.defDb;
         v.altDb = Driver.connect(v.defDb._url + " options='-c search_path=alt'", 'alt');
         v.altDb.query('CREATE SCHEMA IF NOT EXISTS alt');
         TH.coreStartTransaction(v.altDb);
       },
 
-      tearDownOnce() {
+      tearDown() {
+        sut.deregisterQueue('foo');
         if (v.altDb) {
           TH.coreRollbackTransaction(v.altDb);
           v.altDb.query("DROP SCHEMA IF EXISTS alt CASCADE");
@@ -81,14 +79,90 @@ drop sequence if exists _message_queue__id_seq;
         }
       },
 
-      setUp() {
-        TH.startTransaction(v.altDb);
-        dbBroker.db = v.defDb;
-      },
+      "test local registerQueue"() {
+        sut.registerQueue({name: 'bar', local: true, action(...args) {
+          v.args = args;
+          v.db = dbBroker.db;
+        }});
 
-      tearDown() {
-        sut.deregisterQueue('foo');
-        TH.rollbackTransaction(v.altDb);
+        sut.registerQueue({name: 'panda', local: true, action(...args) {
+        }});
+
+        stub(koru, 'setTimeout').onCall(0).returns(123).onCall(1).returns(456);
+        stub(koru, 'clearTimeout');
+
+        sut.getQueue('bar').add({message: 'hello'});
+        sut.getQueue('panda').add({message: 'p1'});
+
+        assert.equals(sut.getQueue('bar').peek()[0].message, 'hello');
+
+        /** with alt db **/
+        dbBroker.db = v.altDb;
+
+        assert.same(sut.getQueue('bar'), undefined);
+
+        sut.registerQueue({name: 'bar', local: true, action(...args) {
+          v.altArgs = args;
+        }});
+
+        assert.same(v.db, undefined);
+        koru.setTimeout.yieldAndReset(); // doesn't matter where yielded;
+        dbBroker.db = v.altDb; // because we stubbed koru.setTimeout
+        assert.same(v.args[0].message, 'hello');
+        assert.same(v.db, v.defDb);
+
+        /** queue to other bar **/
+        sut.getQueue('bar').add({message: 'alt hello'});
+        koru.setTimeout.yieldAndReset();
+
+        assert.equals(v.altArgs[0].message, 'alt hello');
+        assert.same(v.db, v.defDb);
+
+        /** back to orig db **/
+        dbBroker.db = v.defDb;
+
+        assert.same(v.args[1], sut.getQueue('bar'));
+
+        sut.getQueue('bar').add({message: 'middle'});
+        koru.setTimeout.yieldAndReset();
+        assert.equals(v.args[0].message, 'middle');
+        sut.getQueue('bar').add({message: 'goodbye'});
+
+        const {table} = sut.getQueue('bar').mqdb;
+
+        /** purge, deregister **/
+        koru.clearTimeout.reset();
+        sut.getQueue('bar').purge();
+        sut.getQueue('panda').deregister();
+
+        assert.same(table.count({name: 'bar'}), 0);
+        assert.calledWith(koru.clearTimeout, 123);
+        assert.same(table.count({name: 'panda'}), 1);
+        assert.calledWith(koru.clearTimeout, 456);
+
+        assert.same(sut.getQueue('bar'), undefined);
+        assert.same(sut.getQueue('panda'), undefined);
+
+        /** can restart deregisterd queue **/
+
+        koru.setTimeout.reset();
+        koru.clearTimeout.reset();
+
+        sut.registerQueue({name: 'panda', local: true, retryInterval: 30*1000, action(msg) {
+          v.msg = msg;
+        }});
+
+        let now = util.dateNow(); intercept(util, 'dateNow', ()=>now);
+
+        assert(sut.getQueue('panda'));
+
+        assert.calledWith(koru.setTimeout, TH.match.func, 30*1000);
+
+        koru.setTimeout.yieldAndReset();
+
+        assert.equals(v.msg.message, 'p1');
+
+
       },
 
       "test start"() {
@@ -130,7 +204,7 @@ drop sequence if exists _message_queue__id_seq;
 
         koru.setTimeout.yield();
 
-        assert.equals(v.foo, [v.altDb, {name: 'foo', dueAt: util.newDate(), message: 'altfoo1'}]);
+        assert.equals(v.foo, [v.altDb, {_id: '1', dueAt: util.newDate(), message: 'altfoo1'}]);
 
         koru.setTimeout.reset();
         dbBroker.db = v.defDb;
@@ -144,12 +218,12 @@ drop sequence if exists _message_queue__id_seq;
         koru.setTimeout.lastCall.yield();
         koru.setTimeout.firstCall.yield();
 
-        assert.equals(v.foo, [v.defDb, {name: 'foo', dueAt: util.newDate(), message: 'foo1'}]);
-        assert.equals(v.bar, [v.defDb, {name: 'bar', dueAt: util.newDate(), message: 'bar1'}]);
+        assert.equals(v.foo, [v.defDb, {_id: '1', dueAt: util.newDate(), message: 'foo1'}]);
+        assert.equals(v.bar, [v.defDb, {_id: '3', dueAt: util.newDate(), message: 'bar1'}]);
 
         koru.setTimeout.lastCall.yield();
 
-        assert.equals(v.foo, [v.defDb, {name: 'foo', dueAt: util.newDate(), message: 'foo2'}]);
+        assert.equals(v.foo, [v.defDb, {_id: '2', dueAt: util.newDate(), message: 'foo2'}]);
       },
 
       "test getQueue"() {
@@ -168,7 +242,7 @@ drop sequence if exists _message_queue__id_seq;
 
         assert(queue);
         assert.same(sut.getQueue('foo'), queue);
-        refute.same(sut.getQueue('bar'), queue);
+        assert.same(sut.getQueue('bar'), undefined);
 
         dbBroker.db = v.altDb;
 
@@ -191,7 +265,38 @@ drop sequence if exists _message_queue__id_seq;
           .onCall(2).returns(123)
         ;
 
-        sut.registerQueue({module, name: 'foo', action(msg) {v.action(msg)}, retryInterval: 300});
+        sut.registerQueue({module, name: 'foo', action(...args) {v.action(...args)}, retryInterval: 300});
+      },
+
+      "test creates table"() {
+        const query = stub(dbBroker.db, 'query').returns([]);
+        sut.getQueue('foo');
+
+        assert.same(query.callCount, 5);
+
+        assert.equals(query.calls[3].args[0], `
+CREATE TABLE "_test_MQ" (
+    _id bigint NOT NULL,
+    name text COLLATE pg_catalog."C" NOT NULL,
+    "dueAt" timestamp without time zone,
+    message jsonb
+);
+
+CREATE SEQUENCE "_test_MQ__id_seq"
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+ALTER TABLE ONLY "_test_MQ" ALTER COLUMN _id
+  SET DEFAULT nextval('_test_MQ__id_seq'::regclass),
+  ADD CONSTRAINT "_test_MQ_pkey" PRIMARY KEY (_id);
+
+
+CREATE UNIQUE INDEX "_test_MQ_name_dueAt__id" ON "_test_MQ"
+  USING btree (name, "dueAt", _id);
+`);
       },
 
       "test add message"() {
@@ -203,13 +308,13 @@ drop sequence if exists _message_queue__id_seq;
          * @param message the message to action
 
          **/
-        v.action = (args)=>{v.args = args};
+        api.protoMethod('add');
+
+        v.action = (...args)=>{v.args = args};
 
         let now = util.dateNow(); intercept(util, 'dateNow', ()=>now);
 
         const queue = sut.getQueue('foo');
-
-        api.protoMethod('add');
 
         queue.add({dueAt: new Date(now+30), message: {my: 'message'}});
         assert.calledWith(koru.setTimeout, TH.match.func, 30);
@@ -217,7 +322,7 @@ drop sequence if exists _message_queue__id_seq;
         assert.calledOnceWith(koru.clearTimeout, 121);
         assert.calledWith(koru.setTimeout, TH.match.func, 10);
 
-        assert.equals(v.defDb.query('select * from _message_queue order by "dueAt"'), [{
+        assert.equals(v.defDb.query('select * from "_test_MQ" order by "dueAt"'), [{
           _id: '2',
           name: 'foo',
           dueAt: new Date(now+10),
@@ -236,15 +341,15 @@ drop sequence if exists _message_queue__id_seq;
         now+=10;
         call.yield();
 
-        assert.equals(v.args, {
-          name: 'foo',
+        assert.equals(v.args, [{
+          _id: '2',
           dueAt: new Date(now),
           message: {another: 'message'},
-        });
+        }, queue]);
 
         assert.calledOnceWith(koru.setTimeout, TH.match.func, 20);
 
-        assert.equals(v.defDb.query('select * from _message_queue order by "dueAt"'), [{
+        assert.equals(v.defDb.query('select * from "_test_MQ" order by "dueAt"'), [{
           _id: '1',
           name: 'foo',
           dueAt: new Date(now+20),
@@ -256,13 +361,55 @@ drop sequence if exists _message_queue__id_seq;
 
         refute.called(koru.setTimeout);
 
-        assert.equals(v.args, {
-          name: 'foo',
+        assert.equals(v.args, [{
+          _id: '1',
           dueAt: new Date(now-10),
           message: {my: 'message'},
-        });
+        }, queue]);
 
-        assert.equals(v.defDb.query('select * from _message_queue order by "dueAt"'), []);
+        assert.equals(v.defDb.query('select * from "_test_MQ" order by "dueAt"'), []);
+      },
+
+      "test peek"() {
+        /**
+         * Look at messages at the front of the queue without removing them
+
+         * @param maxResults the maximum number of messages to return. Defaults to 1.
+
+         * @param dueAt if given limit resonses to at or before `dueAt`.
+
+         * @returns an array of messages in queue order
+         **/
+        api.protoMethod('peek');
+
+        let now = util.dateNow(); intercept(util, 'dateNow', ()=>now);
+
+        const queue = sut.getQueue('foo');
+
+        queue.add({dueAt: new Date(now+30), message: {my: 'message'}});
+        queue.add({dueAt: new Date(now+10), message: {another: 'message'}});
+
+        assert.equals(queue.peek(), [{
+          _id: '2',
+          dueAt: new Date(now+10),
+          message: {another: 'message'},
+        }]);
+
+        assert.equals(queue.peek(3), [{
+          _id: '2',
+          dueAt: new Date(now+10),
+          message: {another: 'message'},
+        }, {
+          _id: '1',
+          dueAt: new Date(now+30),
+          message: {my: 'message'},
+        }]);
+
+        assert.equals(queue.peek(5, new Date(now+10)), [{
+          _id: '2',
+          dueAt: new Date(now+10),
+          message: {another: 'message'},
+        }]);
       },
 
       "test bad queue Time"() {
@@ -313,6 +460,17 @@ drop sequence if exists _message_queue__id_seq;
 
         koru.setTimeout.lastCall.yield();
         assert.equals(v.args.message, [1,2,3]);
+      },
+
+      "test retryAfter"() {
+        const queue = sut.getQueue('foo');
+        v.action = (args)=>{
+          throw {retryAfter: 12345};
+        };
+
+        queue.add({message: [1,2]});
+        koru.setTimeout.yieldAndReset();
+        assert.calledWith(koru.setTimeout, TH.match.func, 12345);
       },
 
       "test delay more than one day"() {
---
+++ app/koru/model/mq-factory.js
@@ -0,0 +1,241 @@
+define(function(require, exports, module) {
+  const koru            = require('koru');
+  const dbBroker        = require('koru/model/db-broker');
+  const util            = require('koru/util');
+
+  const {hasOwn} = util;
+
+  const timer$ = Symbol(), retryInterval$ = Symbol(),
+        action$ = Symbol(), dbs$ = Symbol(), queue$ = Symbol();
+
+  const DEFAULT_INTERVAL = 60*1000;
+
+  const TABLE_SCHEMA = `
+CREATE TABLE "_message_queue" (
+    _id bigint NOT NULL,
+    name text COLLATE pg_catalog."C" NOT NULL,
+    "dueAt" timestamp without time zone,
+    message jsonb
+);
+
+CREATE SEQUENCE "_message_queue__id_seq"
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+ALTER TABLE ONLY "_message_queue" ALTER COLUMN _id
+  SET DEFAULT nextval('_message_queue__id_seq'::regclass),
+  ADD CONSTRAINT "_message_queue_pkey" PRIMARY KEY (_id);
+
+
+CREATE UNIQUE INDEX "_message_queue_name_dueAt__id" ON "_message_queue"
+  USING btree (name, "dueAt", _id);
+`;
+
+  const fields = {
+    _id: 'bigserial',
+    name: 'text',
+    dueAt: 'timestamp',
+    message: 'jsonb',
+  };
+
+  const action = mq=>{
+    const {name, [timer$]: timer,
+           mqdb: {[action$]: actions, [retryInterval$]: retryIntervals}} = mq;
+    timer.handle = -1; timer.ts = 0;
+    mq.error = undefined;
+    try {
+      const rec = mq.peek(1, util.newDate())[0];
+      if (rec !== undefined) {
+        const {table} = mq.mqdb;
+        actions[name]({_id: rec._id, dueAt: rec.dueAt, message: rec.message}, mq);
+        table._client.query(`delete from "${table._name}" where _id = $1`, [rec._id]);
+      }
+
+      queueNext(mq);
+    } catch(ex) {
+      const retryInterval = typeof ex.retryAfter === 'number'
+            ? ex.retryAfter : retryIntervals[name] || DEFAULT_INTERVAL;
+      if (ex.stack) koru.unhandledException(ex);
+      if (retryInterval !== -1) {
+        timer.handle = 0;
+        queueFor(mq, new Date(util.dateNow()+retryInterval));
+        mq.error = ex;
+      }
+    }
+  };
+
+  const queueNext = (mq, minStart)=>{
+    const {mqdb: {table}, name, [timer$]: timer} = mq;
+
+    const nrec = table._client.query(`
+select "dueAt" from "${table._name}" where name = $1
+  order by "dueAt",_id limit 1;
+`, [name])[0];
+
+      timer.handle = 0;
+      if (nrec !== undefined) {
+        queueFor(mq, minStart === undefined ? nrec.dueAt : Math.max(+minStart, +nrec.dueAt));
+      }
+  };
+
+  const queueFor = (mq, dueAt)=>{
+    if (mq.error !== undefined) return;
+    const ts = +dueAt;
+    if (ts !== ts || ts <= 0) throw new Error("Invalid dueAt");
+    const timer = mq[timer$];
+    if (timer.handle !== 0) {
+      if (timer.ts > ts) {
+        koru.clearTimeout(timer.handle);
+        timer.handle = 0;
+      } else
+        return;
+    }
+    if (timer.handle === 0) {
+      timer.ts = ts;
+      timer.handle = koru.setTimeout(()=>{
+        dbBroker.db = mq.mqdb.table._client;
+        action(mq);
+      }, Math.min(util.DAY, Math.max(0, +dueAt-util.dateNow())));
+    }
+  };
+
+  class MQ {
+    constructor(mqdb, name) {
+      this.mqdb = mqdb;
+      this.name = name;
+      this[timer$] = {handle: 0, dueAt: 0};
+      this.error = undefined;
+      const retryInterval = this.mqdb[retryInterval$][name];
+      queueNext(this, (retryInterval === undefined ? DEFAULT_INTERVAL : retryInterval) + util.dateNow());
+    }
+
+    deregister() {
+      const {name} = this;
+      const {[action$]: actions, [retryInterval$]: retryIntervals} = this.mqdb;
+      const timer = this[timer$];
+
+      if (timer.handle !== 0) {
+        koru.clearTimeout(timer.handle);
+        timer.handle = 0;
+      }
+
+      if (hasOwn(actions, name)) {
+        delete actions[name];
+        if (hasOwn(retryIntervals, name)) delete retryIntervals[name];
+      }
+
+      delete this.mqdb[queue$][name];
+    }
+
+    purge() {
+      this.deregister();
+      this.mqdb.table.remove({name: this.name});
+    }
+
+    add({dueAt=util.newDate(), message}) {
+      const {mqdb, name} = this;
+      const now = util.dateNow();
+      mqdb.table.insert({name, dueAt, message});
+      queueFor(this, dueAt);
+    }
+
+    peek(maxResults=1, dueBefore) {
+      const extra = dueBefore === undefined ? '' : `and "dueAt" <= '${dueBefore.toISOString()}'`;
+      const {table} = this.mqdb;
+      return table._client.query(`
+select _id,"dueAt",message from "${table._name}"
+  where name = $1 ${extra}
+  order by "dueAt",_id limit ${maxResults};
+`, [this.name]);
+    }
+  }
+
+  class MQDB {
+    constructor(tableName, actions, retryIntervals) {
+      const {db} = dbBroker;
+      this[action$] = Object.create(actions);
+      this[retryInterval$] = Object.create(retryIntervals);
+      const table = this.table = db.table(tableName, fields);
+      table._ensureTable();
+      if (table._columns.length == 0) {
+        db.query(TABLE_SCHEMA.replace(/_message_queue/g, tableName));
+      }
+
+      this[queue$] = {};
+    }
+    getQueue(name) {
+      if (this[action$][name] === undefined) return;
+      return this[queue$][name] || (this[queue$][name] = new MQ(this, name));
+    }
+
+    stop() {
+      const queue = this[queue$];
+      for (const name in queue) {
+        const timers = queue[name][timer$];
+        if (timers.handle !== 0) {
+          koru.clearTimeout(timers.handle);
+          timers.handle = timers.ts = 0;
+        }
+      }
+    }
+
+    registerQueue({name, action, retryInterval}) {
+      const {[action$]: actions, [retryInterval$]: retryIntervals} = this;
+      if (hasOwn(actions, name))
+        throw new Error(`Message queue '${name}' already registered`);
+      actions[name] = action;
+      if (retryInterval !== undefined)
+        retryIntervals[name] = retryInterval;
+    }
+  }
+
+  class MQFactory {
+    constructor(tableName) {
+      this.tableName = tableName;
+      this[dbs$] = dbBroker.makeFactory(
+        MQDB,
+        tableName,
+        this[action$] = Object.create(null),
+        this[retryInterval$] = Object.create(null)
+      );
+    }
+
+    start() {
+      const mqdb = this[dbs$].current;
+      for (const name in mqdb[action$]) {
+        mqdb.getQueue(name);
+      }
+    }
+
+    stopAll() {
+      this[dbs$].stop();
+    }
+
+    registerQueue({module, name, action, retryInterval, local=false}) {
+      if (local) {
+        this[dbs$].current.registerQueue({name, action, retryInterval});
+      } else {
+        if (this[action$][name] !== undefined)
+          throw new Error(`MessageQueue '${name}' already registered`);
+        this[action$][name] = action;
+        if (retryInterval !== undefined)
+          this[retryInterval$][name] = retryInterval;
+        module === undefined || module.onUnload(()=>{this.deregisterQueue(name)});
+      }
+    }
+
+    deregisterQueue(name) {
+      delete this[action$][name];
+      delete this[retryInterval$][name];
+    }
+
+    getQueue(name) {
+      return this[dbs$].current.getQueue(name);
+    }
+  };
+
+  return MQFactory;
+});
