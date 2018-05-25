05ed4330f1c3b81d5722058a26e7811b4d77dba4
Geoff Jacobsen <geoffjacobsen@gmail.com>
2018-05-25 12:29:21 +1200

    update docs and wizards

---
+++ skel/newApp/app/models/model.js
@@ -0,0 +1,17 @@
+define(function(require, exports, module) {
+  const Model           = require('koru/model');
+  const Val             = require('koru/model/validation');
+
+  Val.register(module, {
+    associated: require('koru/model/validators/associated-validator'),
+    validate: require('koru/model/validators/validate-validator'),
+    inclusion: require('koru/model/validators/inclusion-validator'),
+    length: require('koru/model/validators/length-validator'),
+    required: require('koru/model/validators/required-validator'),
+    text: require('koru/model/validators/text-validator'),
+    richText: require('koru/ui/rich-text-validator'),
+    unique: require('koru/model/validators/unique-validator'),
+  });
+
+  return Model;
+});
--- skel/newModel/model.js
+++ skel/newModel/model.js
@@ -1,14 +1,14 @@
 define(function(require, exports, module) {
   const Model           = require('models/model');
 
+  const fields = {
+    $$modelFields$$,
+  };
+
   class $$modelName$$ extends Model.BaseModel {
   }
-  $$modelName$$.define({
-    module,
-    fields: {
-      $$modelFields$$,
-    },
-  });
+
+  $$modelName$$.define({module, fields});
 
   return $$modelName$$;
 });
