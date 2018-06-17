4dc54935032c535804c253c3bfbe63f6efe36097
Geoff Jacobsen <geoffjacobsen@gmail.com>
2018-06-18 11:35:08 +1200

    doc acc-sha256

--- app/koru/srp/acc-sha256-test.js
+++ app/koru/srp/acc-sha256-test.js
@@ -1,23 +1,62 @@
 define(function (require, exports, module) {
-  const TH   = require('koru/test');
+  /**
+   * This is useful for building a hash in small increments; it is not the same as hashing the
+   * entire string in one go.
+   **/
+  const TH              = require('koru/test');
+  const api             = require('koru/test/api');
 
   const sut  = require('./acc-sha256');
 
-  TH.testCase(module, {
-    "test accumulation"() {
+  TH.testCase(module, ({beforeEach, afterEach, group, test})=>{
+    test("add", ()=>{
       /**
-       * This is useful for building a hash in small increments; it is not the same as hashing the
-       * entire string in one go.
+       * Modify `hash` using `text`.
+       *
+       * @param text The message to add to `hash`
+       *
+       * @param hash should be an array of 8 32bit integers. The default is a good set.
+       *
+       * @return the modified `hash`; not a copy.
        **/
+      api.method();
+      //[
       const h = [1,2,3,4,5,6,7,8];
       assert.same(sut.add('hello world', h), h);
+      assert.equals(
+        h,
+        [4138495084, 3973010320, 2777164054, 2207796612,
+         615005229, 3241153105, 1397076350, 2212452408]);
+
+      assert.equals(
+        sut.add('secret'),
+        [5028449619, 6360507363, 2345861985, 2860865158,
+         3185633997, 6197280502, 2724194683, 4113015387]);
+      //]
+    });
+
+    test("toHex", ()=>{
+      /**
+       * Convert a `hash` to a hex string.
+       *
+       * @param hash an array of 32bit integers. Usually produced from {#.add}.
+       *
+       * @return the hex equivalent of the `hash`
+       **/
+      api.method();
+      const h = [1,2,3,4,5,6,7,8];
+      assert.same(sut.toHex([65535,1,15,256,0xffffffff,10]),
+                  '0000ffff000000010000000f00000100ffffffff0000000a'),
+
+      sut.add('hello world', h);
 
       assert.same(sut.toHex(sut.add('\na bit more text\n\x01\xf7\x00\n\n\n', h)),
                   '4cb301ea6c1e975ad8130be3e660b5a9ccf9e28e514a73c63533f2690c866255');
+
+      api.done();
       sut.add("363", h);
       assert.same(sut.toHex(sut.add('Ჾ蠇', h)),
                   'f8165b4e4696d5f09d0a08ed60f3503b9f4b15bf5bec95ad1fd7c85a43b00ead');
-    },
-
+    });
   });
 });
--- app/koru/srp/acc-sha256.js
+++ app/koru/srp/acc-sha256.js
@@ -1,4 +1,4 @@
-define(function(require, exports, module) {
+define((require, exports, module)=>{
   const K = [ 0x428A2F98, 0x71374491, 0xB5C0FBCF, 0xE9B5DBA5,
               0x3956C25B, 0x59F111F1, 0x923F82A4, 0xAB1C5ED5,
               0xD807AA98, 0x12835B01, 0x243185BE, 0x550C7DC3,
@@ -27,8 +27,7 @@ define(function(require, exports, module) {
   const u32 = new Uint32Array(ab);
 
   const core_sha256 = (
-    m, l, H=[0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A,
-             0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19 ]
+    m, l, H
   ) => {
     let a, b, c, d, e, f, g, h, i, j, t1, t2;
 
@@ -71,15 +70,18 @@ define(function(require, exports, module) {
   };
 
   return {
-    add: (s, h) => {
-      const ab = utf8Encode(s);
-      return core_sha256(toBinb(ab), ab.length * 8, h);
+    add: (
+      text, hash=[0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A,
+                  0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19 ]
+    ) => {
+      const ab = utf8Encode(text);
+      return core_sha256(toBinb(ab), ab.length * 8, hash);
     },
 
-    toHex: code => {
+    toHex: hash => {
       let hex = '';
-      for(let i = 0; i < code.length; ++i) {
-        u32[0] = code[i];
+      for(let i = 0; i < hash.length; ++i) {
+        u32[0] = hash[i];
         const s = dv.getUint32(0, true).toString(16);
         hex += `${zero.slice(0, 8-s.length)}${s}`;
       }
