d36b056f92cdcbd2f9d086c196e9e89017306744
Geoff Jacobsen <geoffjacobsen@gmail.com>
2018-08-16 17:22:44 +1200

    add ui/dom-undo

---
+++ app/koru/ui/dom-undo-test.js
@@ -0,0 +1,505 @@
+isClient && define((require, exports, module)=>{
+  const Dom             = require('koru/dom');
+  const TH              = require('./test-helper');
+
+  const {test$} = require('koru/symbols');
+
+  const {stub, spy, onEnd, util, match: m} = TH;
+
+  const DomUndo = require('./dom-undo');
+
+  const htj = Dom.htmlToJson;
+
+  const {range$} = DomUndo[test$];
+
+  TH.testCase(module, ({before, after, beforeEach, afterEach, group, test})=>{
+    let input;
+    beforeEach(()=>{
+      input = Dom.h({div: [], contenteditable: true});
+      Dom.setCtx(input);
+      document.body.appendChild(input);
+
+    });
+    afterEach(()=>{
+      TH.domTearDown();
+      input = undefined;
+    });
+
+    test("no change", ()=>{
+      const undo = new DomUndo(input);
+      const changed = stub();
+      onEnd(undo.onChange(changed));
+
+      const tn = document.createTextNode("hello");
+      input.appendChild(tn);
+      tn.remove();
+      undo.recordNow();
+
+      assert.equals(undo.undos, []);
+      assert.equals(undo.redos, []);
+
+      assert.isFalse(undo.undo());
+      assert.isFalse(undo.redo());
+      refute.called(changed);
+    });
+
+    test("add then attribute change", ()=>{
+      const n = document.createElement("br");
+
+      const undo = new DomUndo(input);
+
+      input.appendChild(n);
+
+      n.setAttribute('title', 'foo');
+
+      undo.undo();
+      undo.redo();
+      undo.undo();
+
+      assert.same(input.firstChild, null);
+
+      undo.redo();
+
+      assert.equals(htj(input).div, {title: 'foo', br: []});
+    });
+
+    test("pause, unpause, onChange", ()=>{
+      const undo = new DomUndo(input);
+      const change = stub();
+      onEnd(undo.onChange(change));
+
+      undo.recordNow();
+
+      refute.called(change);
+
+      input.appendChild(Dom.h("1"));
+
+      undo.recordNow();
+
+      assert.calledWith(change, undo);
+      change.reset();
+
+      assert.isFalse(undo.paused);
+      undo.pause();
+      assert.isTrue(undo.paused);
+
+      input.appendChild(Dom.h({i: "2"}));
+      undo.recordNow();
+      input.appendChild(Dom.h({b: "3"}));
+      undo.recordNow();
+      refute.called(change);
+
+      input.appendChild(Dom.h({i: "4"}));
+
+      undo.unpause();
+      assert.isFalse(undo.paused);
+      assert.calledOnceWith(change, undo);
+      change.reset();
+
+      undo.undo();
+      assert.equals(htj(input).div, ['1']);
+      assert.calledOnceWith(change, undo);
+      change.reset();
+
+      undo.redo();
+      assert.equals(htj(input).div, ['1', {i: "2"}, {b: "3"}, {i: "4"}] );
+      assert.calledOnceWith(change, undo);
+      change.reset();
+    });
+
+    test("atomic add, change, remove", ()=>{
+      const undo = new DomUndo(input);
+      const tn = document.createTextNode("1");
+      input.appendChild(tn);
+
+      undo.recordNow();
+      assert.equals(undo.undos, [[
+        {node: m.is(tn), remove: true},
+      ]]);
+
+      tn.textContent = '2';
+      undo.recordNow();
+      assert.equals(undo.undos, [[
+        {node: m.is(tn), remove: true},
+      ],[
+        {node: m.is(tn), text: '1'},
+      ]]);
+
+      tn.remove();
+      undo.recordNow();
+      assert.equals(undo.undos, [[
+        {node: m.is(tn), remove: true},
+      ],[
+        {node: m.is(tn), text: '1'},
+      ],[
+        {node: m.is(tn), parent: m.is(input), before: null},
+      ]]);
+
+      // add-change-remove
+      input.appendChild(tn);
+      tn.textContent = '3';
+      tn.remove();
+
+      undo.recordNow();
+
+      undo.undo();
+      assert.equals(htj(input).div, ['2']);
+    });
+
+    test("attribute change", ()=>{
+      const n = document.createElement("b");
+      n.textContent = "bold";
+      input.appendChild(n);
+
+      const undo = new DomUndo(input);
+
+      n.setAttribute("data-foo", "abc");
+      n.style.setProperty('margin-left', '10px');
+      n.setAttributeNS("my-ns", "x1", 'abc');
+
+      undo.recordNow();
+
+      n.style.setProperty('margin-left', '15px');
+      n.removeAttribute('data-foo');
+
+      undo.recordNow();
+
+      assert.equals(undo.undos, [
+        [{node: m.is(n), attrs: {
+          '': {
+            'data-foo': null,
+            style: null,
+          },
+          'my-ns': {
+            x1: null
+          }}}],
+        [{node: m.is(n), attrs: {
+          '': {
+            'data-foo': 'abc',
+            'style': 'margin-left: 10px;',
+          }
+        }
+        }]
+      ]);
+
+      undo.undo();
+
+      assert.equals(htj(n), {
+        b: ['bold'],
+        style: 'margin-left: 10px;',
+        x1: 'abc',
+        'data-foo': 'abc',
+      });
+
+      undo.undo();
+
+       assert.equals(htj(n), {
+        b: 'bold',
+       });
+
+      assert.same(n.getAttributeNS("my-ns", 'x1'), null);
+
+      undo.redo();
+
+      assert.equals(htj(n), {
+        b: ['bold'],
+        style: 'margin-left: 10px;',
+        x1: 'abc',
+        'data-foo': 'abc',
+      });
+
+      undo.redo();
+
+      assert.equals(htj(n), {
+        b: ['bold'],
+        style: 'margin-left: 15px;',
+        x1: 'abc',
+      });
+
+      assert.same(n.getAttributeNS("my-ns", 'x1'), 'abc');
+
+      n.setAttribute("data-foo", 'xyz');
+
+      n.remove();
+
+      undo.undo();
+      undo.undo();
+
+      assert.same(n.getAttribute('data-foo'), 'abc');
+
+      undo.redo();
+      undo.redo();
+
+      assert.same(n.getAttribute('data-foo'), 'xyz');
+    });
+
+    test("combines small text changes", ()=>{
+      const tn = document.createTextNode("hello");
+      input.appendChild(tn);
+
+      input.focus();
+      TH.setRange(tn, 5);
+      const undo = new DomUndo(input);
+
+      tn.textContent = "hello world";
+      undo.recordNow();
+      tn.textContent = "hello earth world";
+      undo.recordNow();
+      TH.setRange(tn, 3);
+      undo.saveCaret();
+
+      assert.equals(undo.undos, [[
+        {node: m.is(tn), text: 'hello'},
+      ]]);
+      assert.equals(undo.undos[0][range$], [m.is(tn), 5]);
+
+
+      tn.textContent = "this is hello earth world big change";
+      undo.recordNow();
+      TH.setRange(tn, 4);
+      undo.saveCaret();
+
+      assert.same(undo.undos.length, 2);
+      assert.equals(undo.undos[1], [
+        {node: m.is(tn), text: 'hello earth world'},
+      ]);
+      assert.equals(undo.undos[1][range$], [m.is(tn), 3]);
+    });
+
+    test("text change", ()=>{
+      const tn = document.createTextNode("hello");
+      input.appendChild(tn);
+
+      input.focus();
+      TH.setRange(tn, 5);
+      const undo = new DomUndo(input);
+
+      tn.textContent = "hello wor";
+      tn.textContent = "hello world";
+      TH.setRange(tn, 11);
+
+      undo.recordNow();
+
+      assert.equals(undo.undos, [[
+        {node: m.is(tn), text: 'hello'},
+      ]]);
+      assert.equals(undo.undos[0][range$], [m.is(tn), 5]);
+      assert.equals(undo.redos, []);
+
+      undo.undo();
+
+      assert.same(tn.textContent, 'hello');
+
+      assert.equals(Dom.getRange().startOffset, 5);
+
+      assert.equals(undo.redos, [[
+        {node: m.is(tn), text: 'hello world'},
+      ]]);
+      assert.equals(undo.redos[0][range$], [m.is(tn), 11]);
+      assert.equals(undo.undos, []);
+
+      undo.redo();
+
+      assert.same(tn.textContent, 'hello world');
+      assert.equals(undo.undos, [[
+        {node: m.is(tn), text: 'hello'},
+      ]]);
+      assert.equals(undo.undos[0][range$], [m.is(tn), 5]);
+      assert.equals(undo.redos, []);
+    });
+
+    test("add remove order", ()=>{
+      const one = Dom.h('one'), br1 = Dom.h({class: 'br1', br: ''}),
+            two = Dom.h('two'), b = Dom.h({b: '2'}), br2 = Dom.h({class: 'br2', br: ''}),
+            three = Dom.h('three');
+
+      const pre = Dom.h({class: 'pre1', pre: [
+        [one, br1, two, b, br2, three]
+      ]});
+      input.append(pre);
+
+      const undo = new DomUndo(input);
+
+      two.remove(); b.remove(); br2.remove();
+
+      input.appendChild(Dom.h([two, b]));
+
+      three.remove();
+
+      input.appendChild(Dom.h({class: 'pre2', pre: [three]}));
+
+      undo.recordNow();
+
+      assert.equals(htj(input).div, [
+        {class: 'pre1', pre: ['one', {class: 'br1', br: ''}]},
+        'two', {b: '2'},
+        {class: 'pre2', pre: 'three'}
+      ]);
+
+      undo.undo();
+
+      assert.equals(htj(input).div, {
+        class: 'pre1', pre: [
+          'one', {class: 'br1', br: ''},
+          'two', {b: '2'}, {class: 'br2', br: ''},
+          'three']});
+
+      undo.redo();
+
+      assert.equals(htj(input).div, [
+        {class: 'pre1', pre: ['one', {class: 'br1', br: ''}]},
+        'two', {b: '2'},
+        {class: 'pre2', pre: 'three'}
+      ]);
+    });
+
+    test("combination", ()=>{
+      const ol = Dom.h({ol: [{li: 'hello'}]});
+      const li = ol.firstChild;
+      const liText = li.firstChild;
+      const br1 = Dom.h({br: ''}), br2 = Dom.h({br: ''});
+      const text2 = Dom.h('t2');
+      const span = Dom.h({span: text2});
+      input.appendChild(ol);
+
+      const undo = new DomUndo(input);
+
+      TH.setRange(liText, 2);
+      undo.saveCaret();
+
+      input.appendChild(br1);
+      liText.textContent = '';
+      liText.remove();
+      li.appendChild(br2);
+      br2.remove();
+      ol.remove();
+      input.insertBefore(span, br1);
+      text2.remove();
+      input.insertBefore(text2, span);
+      span.remove();
+
+      assert.equals(htj(input).div, ['t2', {br: ''}]);
+
+      undo.undo();
+
+      assert.equals(htj(input).div, {ol: {li: 'hello'}});
+      const range = Dom.getRange();
+      assert.isTrue(range.collapsed);
+      assert.same(range.startContainer, liText);
+      assert.same(range.startOffset, 2);
+
+      undo.redo();
+
+      assert.equals(htj(input).div, ['t2', {br: ''}]);
+    });
+
+    test("node insert", ()=>{
+      const undo = new DomUndo(input);
+
+      const n = document.createElement("b");
+      n.textContent = "bold";
+      input.appendChild(n);
+
+      undo.recordNow();
+
+      assert.equals(undo.undos, [[{node: m.is(n), remove: true}]]);
+      assert.equals(undo.redos, []);
+
+      assert.isTrue(undo.undo());
+
+      assert.same(input.firstChild, null);
+
+      assert.equals(undo.redos, [[{node: m.is(n), parent: m.is(input), before: null}]]);
+      assert.equals(undo.undos, []);
+
+      assert.isTrue(undo.redo());
+
+      assert.dom(input, ()=>{
+        assert.dom('b', 'bold');
+      });
+      assert.equals(undo.undos, [[{node: m.is(n), remove: true}]]);
+      assert.equals(undo.redos, []);
+
+
+      const n2 = document.createElement("i");
+      input.appendChild(n2);
+      n2.textContent = "italic";
+
+      input.insertBefore(n2, n);
+
+      undo.recordNow();
+
+      undo.undo();
+
+      assert.equals(htj(input).div, {b: 'bold'});
+
+      undo.redo();
+
+      assert.equals(htj(input).div, [{i: 'italic'}, {b: 'bold'}]);
+
+      undo.undo();
+      undo.undo();
+      assert.equals(htj(input).div, undefined);
+      undo.redo();
+
+      assert.equals(htj(input).div, {b: 'bold'});
+
+      undo.redo();
+
+      assert.equals(htj(input).div, [{i: 'italic'}, {b: 'bold'}]);
+
+      undo.undo();
+
+      assert.equals(htj(input).div, {b: 'bold'});
+
+      input.appendChild(Dom.h('text'));
+
+      undo.recordNow();
+
+      undo.redo();
+
+      assert.equals(htj(input).div, [{b: 'bold'}, 'text']);
+    });
+
+    test("saveCaret", ()=>{
+      /**
+       * Save the caret so that an undo will restore its position. The caret is only saved if there
+       * are no pending content modifications and is therefore safe to call anytime.
+       **/
+      const tn = document.createTextNode("hello");
+      input.appendChild(tn);
+      TH.setRange(tn, 2);
+
+      const undo = new DomUndo(input);
+
+      tn.textContent = "hello 2";
+      undo.saveCaret();
+      const br = Dom.h({br: 2});
+      input.appendChild(br);
+      TH.setRange(tn, 4);
+      undo.saveCaret();
+
+      undo.recordNow();
+      const ans0 = [
+        {node: m.is(tn), text: 'hello'},
+        {node: m.is(br), remove: true},
+      ];
+      assert.equals(undo.undos, [ans0]);
+      assert.equals(undo.undos[0][range$], [m.is(tn), 2]);
+      TH.setRange(tn, 1);
+      undo.saveCaret();
+
+      TH.setRange(tn, 5);
+      undo.saveCaret();
+
+      tn.textContent = 'moved at 5';
+      undo.recordNow();
+      assert.equals(undo.undos[0], ans0);
+
+      assert.equals(undo.undos[1], [
+        {node: m.is(tn), text: 'hello 2'},
+      ]);
+      assert.equals(undo.undos[1][range$], [m.is(tn), 5]);
+    });
+  });
+});
---
+++ app/koru/ui/dom-undo.js
@@ -0,0 +1,334 @@
+define((require)=>{
+  const Dom             = require('koru/dom');
+  const makeSubject     = require('koru/make-subject');
+  const util            = require('koru/util');
+
+  const {test$} = require('koru/symbols');
+
+  const {hasOwn} = util;
+
+  const mo$ = Symbol(), paused$ = Symbol(), onchange$ = Symbol(),
+        redos$ = Symbol(), undos$ = Symbol(), pendingTail$ = Symbol(),
+        pending$ = Symbol(), range$ = Symbol(), target$ = Symbol();
+
+  const OB_ALL = {
+    subtree: true, childList: true,
+    attributes: true, attributeOldValue: true,
+    characterData: true,
+    characterDataOldValue: true,
+  };
+
+  const setAttr = ({attributeNamespace, attributeName, oldValue}, value)=>{
+    const ans = attributeNamespace  || '';
+    const attrs = value[ans] || (value[ans] = {});
+    attrs[attributeName] = oldValue;
+    return value;
+  };
+
+  const notify = (undo)=>{undo[onchange$].notify(undo)};
+
+  // // test helpers
+  // let gid = 0;
+
+  // const db = (node)=>{
+  //   if (node == null) return node;
+  //   if (node[test$]) return node[test$];
+  //   return node[test$] = `${++gid}:${node.tagName || node.nodeValue}`;
+  // };
+
+  const SUM_ACTIONS = {
+    characterData: (list, mut)=>{
+      const {target} = mut;
+      const action = list.getAction(target);
+      if (action === undefined) {
+        list.addAction(target, {node: target, text: mut.oldValue});
+      } else if (action.text === undefined)
+        action.text = mut.oldValue;
+    },
+
+    childList: (list, mut)=>{
+      const {addedNodes, removedNodes, target} = mut;
+      for (const node of addedNodes) {
+        const action = list.getAction(node);
+        if (node.parentNode !== null)
+          ++list.cc;
+        else
+          list.limbo.add(node);
+        list.pending.push({node, remove: true});
+      }
+      for (const node of removedNodes) {
+        if (node.parentNode === null) {
+          if (list.limbo.has(node))
+            list.limbo.delete(node);
+          else
+            ++list.cc;
+        }
+        const action = list.getAction(node);
+        list.pending.push({node, parent: target, before: mut.nextSibling});
+      }
+    },
+
+    attributes: (list, mut)=>{
+      const {target} = mut;
+      const action = list.getAction(target);
+      if (action === undefined) {
+        list.addAction(target, {node: target, attrs: setAttr(mut, {})});
+      } else if (action.attrs === undefined)
+        action.attrs = setAttr(mut, {});
+      else
+        setAttr(mut, action.attrs);
+    },
+  };
+
+  const applyAttrs = (node, attrs) =>{
+    for (const ns in attrs) {
+      const nsmap = attrs[ns];
+      for (const name in nsmap) {
+        const v = nsmap[name];
+        if (v == null)
+          node.removeAttributeNS(ns, name);
+        else
+          node.setAttributeNS(ns, name, v);
+      }
+    }
+  };
+
+  const debugMut = (m)=> util.inspect(
+    m.type === 'childList'
+      ? {target: m.target, addedNodes: Array.from(m.addedNodes),
+         removedNodes: Array.from(m.removedNodes)}
+    : {target: m.target, oldValue: m.oldValue}
+  );
+
+
+  const summarise = (undo, list, muts)=>{
+    if (muts.length == 0 && muts[pending$] === undefined)
+      return;
+
+    const range = undo[range$];
+    saveCaret(undo);
+    const ans = list.record(muts);
+    if (ans === undefined) return false;
+    if (range !== undefined && ans[range$] === undefined)
+      ans[range$] = range;
+    return true;
+  };
+
+  const applyActions = (undo, undos, redos)=>{
+    undo.recordNow();
+    const actions = undos.pop();
+    if (actions === undefined) return false;
+
+    for(let i = actions.length-1; i >= 0; --i) {
+      const action = actions[i];
+      const {node, text, attrs, remove} = action;
+      if (text !== undefined) {
+        node.textContent = text;
+      }
+      if (attrs !== undefined) {
+        applyAttrs(node, attrs);
+      }
+      if (remove) {
+        node.remove();
+      } else {
+        const {parent} = action;
+        if (parent !== undefined) {
+          parent.insertBefore(node, action.before);
+        }
+      }
+    }
+
+    const sel = actions[range$];
+    if (sel !== undefined) {
+      const range = document.createRange();
+      range.setStart(sel[0], sel[1]);
+      sel.length == 4 && range.setEnd(sel[2], sel[3]);
+      Dom.setRange(range);
+    }
+
+    summarise(undo, redos, undo[mo$].takeRecords());
+    notify(undo);
+    return true;
+  };
+
+  const saveCaret = (undo, range=Dom.getRange())=>{
+    if (range !== null && undo[target$].contains(range.startContainer)) {
+
+      const sel = [range.startContainer, range.startOffset];
+      range.collapsed || sel.push(range.endContainer, range.endOffset);
+      undo[range$] = sel;
+    }
+  };
+
+  const newPending = ()=>{
+    const ans = [];
+    ans[target$] = new Map();
+    return ans;
+  };
+
+  const copyActionFields = (to, from)=>{
+    if (from.text !== undefined) to.text = from.text;
+    if (from.attrs !== undefined) to.attrs = from.attrs;
+  };
+
+
+  class UndoList {
+    constructor(isUndo) {
+      this.cast = new Map();
+      this.isUndo = isUndo;
+      this.actions = [];
+      this.pending = newPending();
+      this.cc = 0;
+      this.limbo = new Set();
+    }
+
+    clear() {
+      this.cast.clear();
+      this.actions.length = 0;
+    }
+
+    pop() {
+      const action = this.actions.pop();
+      return action;
+    }
+
+    getAction(node, slot=this.pending) {return slot[target$].get(node)}
+
+    addAction(node, action, slot=this.pending) {
+      if (node.parentNode !== null) ++this.cc;
+      const {parent} = action;
+      slot[target$].set(node, action);
+      slot.push(action);
+    }
+
+    get last() {
+      const {actions} = this;
+      return actions.length == 0 ? null : actions[actions.length-1];
+    }
+
+    record(muts) {
+      for (;muts !== undefined; muts = muts[pending$]) {
+        for (const mut of muts) {
+          SUM_ACTIONS[mut.type](this, mut);
+        }
+      }
+      const {pending} = this;
+      if (pending.length == 0) return;
+      this.pending = newPending();
+      const {cc} = this;
+      this.limbo.clear();
+      this.cc = 0;
+
+      if (cc === 0) {
+        const {last} = this;
+        last === null || util.append(last, pending);
+        return;
+      }
+
+      if (this.isUndo && pending.length == 1) {
+        const curr = pending[0];
+        if (curr.remove === undefined && curr.parent === undefined && curr.attrs === undefined) {
+          const {last} = this;
+          if (last !== null && last.length == 1) {
+            const prev = last[0];
+            if (prev.text !== undefined && util.diffStringLength(prev.text, curr.text) < 10) {
+              return;
+            }
+          }
+        }
+      }
+
+      this.actions.push(pending);
+      return pending;
+    }
+  }
+
+  class DomUndo {
+    constructor(target) {
+      this[target$] = target;
+      this[undos$] = new UndoList(true);
+      this[redos$] = new UndoList(false);
+      this[range$] = this[pending$] = this[pendingTail$] = undefined;
+      this[mo$] = new window.MutationObserver(muts => this.recordNow(muts));
+      const ctx = Dom.ctx(target);
+      ctx === null || ctx.onDestroy(()=>{this.disconnect()});
+      this.reconnect();
+      this[onchange$] = makeSubject();
+      this[paused$] = false;
+    }
+
+    onChange(subject) {return this[onchange$].onChange(subject)}
+
+    recordNow(muts=this[mo$].takeRecords()) {
+      if (this[paused$] || this[pending$] !== undefined) {
+        if (muts.length != 0) {
+          if (this[pending$] === undefined)
+            this[pending$] = this[pendingTail$] = muts;
+          else
+            this[pendingTail$] = this[pendingTail$][pending$] = muts;
+        }
+        if (this[paused$]) return;
+        muts = this[pending$];
+        this[pending$] = this[pendingTail$] = undefined;
+      }
+      if (muts.length != 0) {
+        this[redos$].clear();
+        if (summarise(this, this[undos$], muts)) {
+          notify(this);
+          return true;
+        }
+      }
+    }
+
+    disconnect() {
+      this[mo$].disconnect();
+      this[undos$].clear(); this[redos$].clear();
+    }
+
+    reconnect() {
+      this[mo$].observe(this[target$], OB_ALL);
+      this.saveCaret();
+    }
+
+    undo() {return applyActions(this, this[undos$], this[redos$])}
+    redo() {return applyActions(this, this[redos$], this[undos$])}
+
+    get undos() {return this[undos$].actions}
+    get redos() {return this[redos$].actions}
+
+    get paused() {return this[paused$]}
+
+    pause() {
+      this[paused$] = true;
+    }
+
+    unpause() {
+      this[paused$] = false;
+      this.recordNow();
+    }
+
+    saveCaret(range=Dom.getRange()) {
+      if (this[pending$] !== undefined)
+        return;
+
+      const muts =this[mo$].takeRecords();
+      if (muts.length != 0) {
+        this[pending$] = this[pendingTail$] = muts;
+        return;
+      }
+
+      saveCaret(this, range);
+    }
+
+    reset() {
+      this.disconnect();
+      this.reconnect();
+    }
+  }
+
+  DomUndo[test$] = {
+    range$
+  };
+
+  return DomUndo;
+});
