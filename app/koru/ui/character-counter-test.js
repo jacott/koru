0ed28f1c92d0c9980e9c98f5df7083c30c45fdee
Geoff Jacobsen <geoffjacobsen@gmail.com>
2018-11-30 16:14:26 +1300

    add ui/character-counter

---
+++ app/koru/ui/character-counter-test.js
@@ -0,0 +1,87 @@
+isClient && define((require, exports, module)=>{
+  const Dom             = require('koru/dom');
+  const PlainText       = require('koru/ui/plain-text');
+  const TH              = require('koru/ui/test-helper');
+
+  const {stub, spy, onEnd, util, match: m, stubProperty, intercept} = TH;
+
+  const CharacterCounter = require('./character-counter');
+
+  const {MutationObserver} = window;
+
+  TH.testCase(module, ({before, after, beforeEach, afterEach, group, test})=>{
+    afterEach(()=>{
+      Dom.removeChildren(document.body);
+    });
+
+    test("attach", ()=>{
+      const editor = PlainText.Editor.$autoRender({content: "foo", options: {
+        placeholder: "hello"}});
+      document.body.appendChild(editor);
+
+      let checkNow;
+      intercept(window, 'MutationObserver', function (func) {
+        checkNow = func;
+        return new MutationObserver(func);
+      });
+
+      const cc = new CharacterCounter({maxlength: 10, warninglength: 6});
+      cc.attach(editor);
+      const counter = cc.element.firstChild;
+      assert.equals(Dom.htmlToJson(cc.element), {class: 'ui-charCounter', div: {div: [
+        {span: "3"}, {span: "10"},
+      ]}});
+
+      editor.textContent = 'over50';
+      let muts = cc.mutationObserver.takeRecords();
+      assert.same(muts.length, 1);
+      checkNow(muts);
+
+      assert.equals(Dom.htmlToJson(counter), {class: 'ui-warn', div: [
+        {span: "6"}, {span: "10"},
+      ]});
+
+      editor.appendChild(Dom.h('hello'));
+      muts = cc.mutationObserver.takeRecords();
+      assert.same(muts.length, 1);
+      checkNow(muts);
+
+      assert.equals(Dom.htmlToJson(counter), {class: 'ui-warn ui-error', div: [
+        {span: "11"}, {span: "10"},
+      ]});
+
+      editor.firstChild.textContent = '';
+
+      cc.checkNow();
+      assert.equals(Dom.htmlToJson(counter), {class: '', div: [
+        {span: "5"}, {span: "10"},
+      ]});
+
+      editor.textContent = '123456789012345';
+      checkNow(muts);
+      assert.equals(Dom.htmlToJson(counter), {class: 'ui-warn ui-error', div: [
+        {span: "15"}, {span: "10"},
+      ]});
+
+      editor.textContent = '1234567890';
+      checkNow(muts);
+      assert.equals(Dom.htmlToJson(counter), {class: 'ui-warn', div: [
+        {span: "10"}, {span: "10"},
+      ]});
+
+      editor.textContent = '1';
+      editor.appendChild(Dom.h({div: ['2', {b: {i: '34'}}, {br: ''}]}));
+
+      muts = cc.mutationObserver.takeRecords();
+      checkNow(muts);
+      assert.equals(Dom.htmlToJson(counter), {class: '', div: [
+        {span: "5"}, {span: "10"},
+      ]});
+
+      spy(cc.mutationObserver, 'disconnect');
+      cc.attach(null);
+      assert.called(cc.mutationObserver.disconnect);
+      assert.same(cc.editor, null);
+    });
+  });
+});
---
+++ app/koru/ui/character-counter.js
@@ -0,0 +1,81 @@
+define((require, exports, module)=>{
+  const Dom             = require('koru/dom');
+
+  const COUNTER = Dom.h({class: 'ui-charCounter', div: {div: [{span: ''}, {span: ''}]}});
+
+  const OB_DataChange = {
+    subtree: true, childList: true,
+    characterData: true,
+  };
+
+  const countText = (editor)=>{
+    let size = 0;
+    for (let node = editor.firstChild; node !== null; node = node.nextSibling) {
+      const {nodeType} = node;
+      if (nodeType === document.TEXT_NODE)
+        size += node.nodeValue.length;
+      else if (nodeType === 1) {
+        if (node.tagName === 'BR') ++size;
+        else size += countText(node);
+      }
+    }
+    return size;
+  };
+
+
+  class CharacterCounter {
+    constructor({maxlength=0, warninglength=Math.floor(maxlength*.9)}) {
+      this.maxlength = maxlength;
+      this.warninglength = warninglength;
+      this.element = COUNTER.cloneNode(true);
+      this.mutationObserver = new window.MutationObserver(muts =>{
+        if (muts.length == 0) return;
+        this.checkNow();
+      });
+      this.editor = null;
+    }
+
+    attach(editor=null) {
+      if (this.editor !== null) this.mutationObserver.disconnect();
+      this.editor = editor;
+      if (editor == null) return;
+
+
+      const counter = this.element.firstChild;
+      counter.classList.remove('ui-error', 'ui-warn');
+      if (this.maxlength != 0) counter.lastChild.textContent = ''+this.maxlength;
+      this.prevSize = 0;
+      this.mutationObserver.observe(editor, OB_DataChange);
+      this.checkNow();
+    }
+
+    checkNow() {
+      if (this.editor === null || this.editor.parentNode === null || Dom.myCtx(this.editor) === null) {
+        this.mutationObserver.disconnect();
+        this.editor = null;
+        return;
+      }
+      const size = countText(this.editor);
+
+      const counter = this.element.firstChild;
+      counter.firstChild.textContent = ''+size;
+
+      const {warninglength, maxlength, prevSize} = this;
+
+      if (warninglength <= size) {
+        warninglength > prevSize && counter.classList.add('ui-warn');
+
+        if (maxlength < size)
+          maxlength >= prevSize && counter.classList.add('ui-error');
+        else
+          maxlength < prevSize && counter.classList.remove('ui-error');
+      } else {
+        warninglength <= prevSize && counter.classList.remove('ui-warn');
+        maxlength < prevSize && counter.classList.remove('ui-error');
+      }
+      this.prevSize = size;
+    }
+  };
+
+  return CharacterCounter;
+});
