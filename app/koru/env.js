117a271dae4b856932aceaa58fd8aa9c82363e5e
Geoff Jacobsen <geoffjacobsen@gmail.com>
2019-01-08 10:05:32 +1300

    env: handle for test files

--- app/koru/env.js
+++ app/koru/env.js
@@ -12,7 +12,7 @@
      * format: `koru/env!name` as `name-client.js`
      */
     load: (name, req, onload, config)=>{
-      const provider = name + suffix;
+      const provider = name.endsWith('-test') ? name.slice(0,-5) + suffix + "-test" : name + suffix;
       const pMod = req.module.dependOn(provider);
       if (pMod === undefined || pMod.isUnloaded()) {
         onload.error("Can't load "+provider);
