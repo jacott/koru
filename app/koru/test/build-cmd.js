8306d68e96e1badb8556641c0f96285b81f7b41f
Geoff Jacobsen <geoffjacobsen@gmail.com>
2021-09-07 17:48:51 +1200

    fix(test): BuildCmd.findAll to not throw on invalid symlinks

--- app/koru/test/build-cmd.js
+++ app/koru/test/build-cmd.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const Path = require('path');
 
-define((require, exports, module)=>{
+define((require, exports, module) => {
   'use strict';
   const fst             = require('koru/fs-tools');
   const koru            = require('../main');
@@ -18,26 +18,33 @@ define((require, exports, module)=>{
       const cTests = type !== 'server' ? [] : null;
       const sTests = type !== 'client' ? [] : null;
 
-      const pushPath = (path)=>{
-        cTests !== null && !path.match(/\bserver\b/i) && cTests.push(path);
-        sTests !== null && !path.match(/\bclient\b|\bui\b/i) && sTests.push(path);
+      const pushPath = (path) => {
+        cTests !== null && ! path.match(/\bserver\b/i) && cTests.push(path);
+        sTests !== null && ! path.match(/\bclient\b|\bui\b/i) && sTests.push(path);
       };
 
-
-      const findAll = (dir, exDirs) =>{
+      const findAll = (dir, exDirs) => {
         const dirPath = Path.join(topDir, dir);
-        const filenames = readdir(dirPath).wait().filter(fn => (
+        const filenames = readdir(dirPath).wait().filter((fn) => (
           (exDirs === undefined || exDirs[fn] === undefined) &&
-            (fn.endsWith("-test.js") || ! fn.endsWith(".js"))));
-        const stats = filenames.map(filename => stat(Path.join(dirPath, filename)));
+            (fn.endsWith('-test.js') || ! fn.endsWith('.js'))));
+        const stats = filenames.map((filename) => stat(Path.join(dirPath, filename)));
 
         wait(stats);
 
-        for(let i = 0; i < filenames.length; ++i) {
-          if (stats[i].get().isDirectory()) {
-            findAll(Path.join(dir, filenames[i]));
-          } else if (filenames[i].endsWith("-test.js")) {
-            pushPath(Path.join(dir,filenames[i].slice(0,-3)));
+        for (let i = 0; i < filenames.length; ++i) {
+          try {
+            if (stats[i].get().isDirectory()) {
+              findAll(Path.join(dir, filenames[i]));
+            } else if (filenames[i].endsWith('-test.js')) {
+              pushPath(Path.join(dir, filenames[i].slice(0, -3)));
+            }
+          } catch (err) {
+            if (err.code === 'ENOENT') {
+              koru.error(err);
+            } else {
+              throw err;
+            }
           }
         }
       };
@@ -52,41 +59,44 @@ define((require, exports, module)=>{
         }
         // all
         const config = module.config();
-        const exDirs = koru.util.toMap(config.excludeDirs||[]);
-        util.forEach(config.testDirs || ["."], dir =>{findAll(dir, exDirs)});
+        const exDirs = koru.util.toMap(config.excludeDirs || []);
+        util.forEach(config.testDirs || ['.'], (dir) => {findAll(dir, exDirs)});
       } else {
         // one
         const idx = pattern.indexOf(' ');
-        pushPath((idx === -1 ? pattern : pattern.slice(0, idx))+'-test');
+        pushPath((idx === -1 ? pattern : pattern.slice(0, idx)) + '-test');
       }
 
       type = 'none';
 
-      if (cTests && cTests.length)
+      if (cTests && cTests.length) {
         type = 'client';
+      }
 
-      if (sTests && sTests.length)
+      if (sTests && sTests.length) {
         type = type === 'none' ? 'server' : 'both';
+      }
 
-      if (type === 'none')
+      if (type === 'none') {
         return;
+      }
 
       if (type !== 'client') {
         const dest = module.toUrl('test/server-ready.js');
         if (module.ctx.modules['test/server-ready']) {
-          BuildCmd.serverReady = new Future;
+          BuildCmd.serverReady = new Future();
           fs.unlinkSync(dest);
           BuildCmd.serverReady.wait();
           BuildCmd.serverReady = null;
         }
         try {
           fs.symlinkSync(module.toUrl('./server-ready-prep.js'), dest);
-        } catch(ex) {}
+        } catch (ex) {}
       }
 
       callback(type, {
         server() {
-          require(['./server', 'test/server-ready'], (TH, serverReady)=>{
+          require(['./server', 'test/server-ready'], (TH, serverReady) => {
             serverReady(koru, BuildCmd);
             TH.run(pattern, sTests);
           });
@@ -94,7 +104,7 @@ define((require, exports, module)=>{
         clientTests: cTests,
         client(conn, clientTests) {
           conn.sendBinary('T', [pattern, clientTests]);
-        }
+        },
       });
     },
   };
