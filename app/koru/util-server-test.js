244e899ad0b48ba09746a95675a5ba80897c7ef4
Geoff Jacobsen <geoffjacobsen@gmail.com>
2021-09-22 16:05:19 +1200

    feat(util): make util.callWait return better error stack trace
    make util.waitCallback return a [err, result] tuple unless unexpected error

--- app/koru/util-server-test.js
+++ app/koru/util-server-test.js
@@ -1,64 +1,66 @@
-define((require, exports, module)=>{
+define((require, exports, module) => {
   'use strict';
-  const api             = require('koru/test/api');
   const TH              = require('koru/test-helper');
+  const api             = require('koru/test/api');
 
   const {stub, spy, intercept} = TH;
 
-  const util  = require('./util');
+  const util = require('./util');
 
-  TH.testCase(module, ({beforeEach, afterEach, group, test})=>{
-    beforeEach(()=>{
+  TH.testCase(module, ({beforeEach, afterEach, group, test}) => {
+    beforeEach(() => {
       api.module({subjectModule: module.get('./util'), subjectName: 'util'});
     });
 
-    test("thread", ()=>{
+    test('thread', () => {
       /**
        * An object associated with the current [Fiber](https://www.npmjs.com/package/fibers).
        **/
       api.property();
       assert.same(util.thread, util.thread);
       let other;
-      util.Fiber(()=>{other = util.thread}).run();
+      util.Fiber(() => {other = util.thread}).run();
       refute.same(util.thread, other);
       assert.equals(util.thread, TH.match.baseObject);
       assert.equals(other, TH.match.baseObject);
     });
 
-    group("waitCallback", ()=>{
+    group('waitCallback', () => {
       let origCallTimeout;
-      beforeEach(()=>{
+      beforeEach(() => {
         stub(global, 'setTimeout').returns(123);
         stub(global, 'clearTimeout');
         origCallTimeout = util.thread.callTimeout;
         util.thread.callTimeout = undefined;
       });
 
-      afterEach(()=>{
+      afterEach(() => {
         util.thread.callTimeout = origCallTimeout;
         origCallTimeout = null;
       });
 
-      test("callback", ()=>{
+      test('callback', () => {
         let resolved = false;
-        const future = {throw: stub(), return: stub(), isResolved: ()=> resolved};
+        const future = {throw: stub(), return: stub(), isResolved: () => resolved};
 
         const func = util.waitCallback(future);
 
         assert.calledWith(setTimeout, TH.match.func, 20*1000);
         refute.called(clearTimeout);
 
-        const err = new Error("foo");
+        const err = new Error('foo');
         func(err);
 
         assert.calledWith(future.throw, err);
         refute.called(future.return);
+        future.throw.reset();
 
-        func(null, "message");
-        assert.calledWith(future.return, "message");
-        assert.calledOnce(future.throw);
+        func(null, 'message');
+        assert.calledOnceWith(future.return, [null, 'message']);
+        refute.called(future.throw);
+        future.return.reset();
         func(123);
-        assert.calledWith(future.throw, TH.match(err => err.message === "123"));
+        assert.calledWith(future.return, [{error: 500, reason: '123'}]);
 
         future.throw.reset(); future.return.reset();
         resolved = true;
@@ -67,41 +69,39 @@ define((require, exports, module)=>{
         refute.called(future.return);
       });
 
-      test("timeout", ()=>{
+      test('timeout', () => {
         util.thread.callTimeout = 10*1000;
         setTimeout.restore();
         const origSetTimeout = setTimeout;
-        intercept(global, 'setTimeout', (func, to)=>{
+        intercept(global, 'setTimeout', (func, to) => {
           assert.same(to, 10*1000);
           origSetTimeout(func, 0);
         });
 
-        const future = new util.Future;
+        const future = new util.Future();
         const func = util.waitCallback(future);
 
-        assert.exception(()=>{
-          future.wait();
-        }, {error: 504, reason: 'Timed out'});
+        assert.equals(future.wait(), [{error: 504, reason: 'Timed out'}]);
 
         refute.called(clearTimeout);
 
-        func(123); // no exception
+        assert.same(func(123), void 0);
       });
     });
 
-    test("callWait", ()=>{
-      const wait = stub().returns("success");
+    test('callWait success', () => {
+      const wait = stub().returns([null, 'success']);
       const future = {wait};
       function myFuture() {return future}
       const method = stub();
-      stub(util, "waitCallback").returns("waitCallback-call");
+      stub(util, 'waitCallback').returns('waitCallback-call');
       const myThis = {method};
 
       intercept(util, 'Future', myFuture);
 
-      assert.same(util.callWait(method, myThis, "foo", 1, 2), "success");
+      assert.same(util.callWait(method, myThis, 'foo', 1, 2), 'success');
 
-      assert.calledWith(method, "foo", 1, 2, "waitCallback-call");
+      assert.calledWith(method, 'foo', 1, 2, 'waitCallback-call');
       assert.same(method.firstCall.thisValue, myThis);
 
       assert.calledWith(util.waitCallback, future);
@@ -109,7 +109,22 @@ define((require, exports, module)=>{
       assert(method.calledBefore(wait));
     });
 
-    test("engine", ()=>{
+    test('callWait error', () => {
+      const wait = stub().returns([{error: 400, reason: 'is_invalid'}]);
+      const future = {wait};
+      function myFuture() {return future}
+      const method = stub();
+      stub(util, 'waitCallback').returns('waitCallback-call');
+      const myThis = {method};
+
+      intercept(util, 'Future', myFuture);
+
+      assert.exception(() => {
+        util.callWait(method, myThis, 'foo', 1, 2);
+      }, {error: 400, reason: 'is_invalid'});
+    });
+
+    test('engine', () => {
       assert.same(util.engine, 'Server');
     });
   });
--- app/koru/util-server.js
+++ app/koru/util-server.js
@@ -1,4 +1,4 @@
-define((require)=>{
+define((require) => {
   'use strict';
   const KoruError       = require('koru/koru-error');
   const util            = require('./util');
@@ -6,12 +6,13 @@ define((require)=>{
   util.engine = 'Server';
   util.Fiber = requirejs.nodeRequire('fibers');
 
-  util.waitCallback = (future, callTimeout=util.thread.callTimeout) => {
-    let cto = callTimeout === 0 ? 0
-        : setTimeout(()=>{
+  util.waitCallback = (future, callTimeout=util.thread.callTimeout ?? 20*1000) => {
+    let cto = callTimeout === 0
+        ? 0
+        : setTimeout(() => {
           cto = 0;
-          future.isResolved() || future.throw(new KoruError(504, 'Timed out'));
-        }, callTimeout === undefined ? 20*1000 : callTimeout);
+          future.isResolved() || future.return([{error: 504, reason: 'Timed out'}]);
+        }, callTimeout);
 
     return (err, response) => {
       if (cto != 0) {
@@ -19,20 +20,26 @@ define((require)=>{
         cto = 0;
       }
       if (future.isResolved()) return;
-      if (err) {
-        if (err instanceof Error)
+      if (err != null) {
+        if (err instanceof Error) {
           future.throw(err);
-        else
-          future.throw(new Error(err.toString()));
-      } else
-        future.return(response);
+        } else {
+          future.return([{error: err.error ?? 500, reason: err.reason ?? err.toString()}]);
+        }
+      } else {
+        future.return([null, response]);
+      }
     };
   };
 
-  util.callWait = (method, caller, ...args)=>{
-    const future = new util.Future;
+  util.callWait = (method, caller, ...args) => {
+    const future = new util.Future();
     method.call(caller, ...args, util.waitCallback(future));
-    return future.wait();
+    const [err, response] = future.wait();
+    if (err != null) {
+      throw new KoruError(err.error, err.reason);
+    }
+    return response;
   };
 
   // Fix fibers making future enumerable
