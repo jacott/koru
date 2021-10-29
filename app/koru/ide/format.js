05a1bd68c1d1ca2daa4ae8e2aa17e602fb9d22e9
Geoff Jacobsen <geoffjacobsen@gmail.com>
2021-10-29 16:29:26 +1300

    feat(ide): show syntax errors when formatting code

--- app/koru/ide/format.js
+++ app/koru/ide/format.js
@@ -1,10 +1,20 @@
 define((require, exports, module) => {
   'use strict';
+  const koru            = require('koru');
   const CodeFormatter   = require('koru/parse/code-formatter');
 
-  const options = {ignoreSyntaxError: true};
+  const options = {ignoreSyntaxError: false};
 
   return (ws, _clients, input) => {
-    ws.send('IF' + JSON.stringify({source: CodeFormatter.reformat(input, options)}));
+    try {
+      ws.send('IF' + JSON.stringify({source: CodeFormatter.reformat(input, options)}));
+    } catch(err) {
+      if (err.name === 'SyntaxError') {
+        koru.error(err.toString());
+      } else {
+        koru.unhandledException(err);
+      }
+      ws.send('IF' + JSON.stringify({source: input}));
+    }
   };
 });
