257a25c75854cf1e3b1434899b5141c8c577cde6
Geoff Jacobsen <geoffjacobsen@gmail.com>
2021-11-29 10:58:13 +1300

    deps: remove request, update ws@8.3.0 postcss@8.4.4 nodemailer@6.7.2 marked@4.0.5

--- app/koru/http-util-test.js
+++ app/koru/http-util-test.js
@@ -1,6 +1,5 @@
-isServer && define((require, exports, module)=>{
+isServer && define((require, exports, module) => {
   'use strict';
-  const request = requirejs.nodeRequire('request');
   const koru            = require('koru');
   const HttpHelper      = require('koru/http-helper');
   const TH              = require('koru/test-helper');
@@ -9,158 +8,21 @@ isServer && define((require, exports, module)=>{
 
   const {stub, spy, intercept, match} = TH;
 
-  const sut  = require('./http-util');
+  const sut = require('./http-util');
 
-  let v = {};
-
-  TH.testCase(module, ({beforeEach, afterEach, group, test})=>{
-    afterEach(()=>{
-      v = {};
-    });
-
-    group("request", ()=>{
-      beforeEach(()=>{
-        v.orig = sut[isTest].request;
-        v.req = stub();
-        sut[isTest].request = v.req;
-      });
-
-      test("throw opts", ()=>{
-        assert.equals(sut.THROW_5XX, {serverError: 'throw'});
-        assert.equals(sut.THROW_NONE, {});
-        assert.equals(sut.THROW_ERROR, {serverError: 'throw', clientError: 'throw'});
-        assert.equals(sut.THROW_ERROR_NOT_404, {
-          serverError: 'throw', clientError: 'throw', 404: 'return'});
-      });
-
-      afterEach(()=>{
-        sut[isTest].request = v.orig;
-      });
-
-      test("setup", ()=>{
-        assert.same(v.orig, request);
-      });
-
-      test("HttpError", ()=>{
-        let response = {statusCode: 403};
-        let error = new sut.HttpError({response, body: 'the body'});
-        assert.same(error.message, 'Bad Request [403]');
-        assert.same(error.statusCode, 403);
-        assert.same(error.response, response);
-        assert.same(error.body, 'the body');
-
-        error = new sut.HttpError({message: 'foo', statusCode: 418, response});
-        assert.same(error.message, 'foo [418]');
-        assert.same(error.statusCode, 418);
-        assert.same(error.response, response);
-        assert.same(error.body, undefined);
-
-
-        error = new sut.HttpError({statusCode: 418});
-        assert.same(error.statusCode, 418);
-        assert.same(error.response, undefined);
-        assert.same(error.body, undefined);
-
-        error = new sut.HttpError();
-        assert.same(error.message, 'Bad Request [400]');
-        assert.same(error.statusCode, 400);
-        assert.same(error.response, undefined);
-        assert.same(error.body, undefined);
-      });
-
-      test("timeout", ()=>{
-        v.req.yields({message: 'Timeout', code: 'ETIMEDOUT'});
-        assert.exception(()=>{sut.request({method: 'HEAD'})}, {
-          message: 'Timeout [504]', statusCode: 504});
-
-        assert.calledWith(v.req, {method: 'HEAD', timeout: 20*1000});
-      });
-
-      test("ECONNREFUSED", ()=>{
-        v.req.yields({message: 'Connection Refused', errno: 'ECONNREFUSED'});
-        assert.exception(
-          ()=>{sut.request({method: 'PUT', timeout: 12345}, {serverError: 'throw'})},
-          {message: 'Connection Refused [503]', statusCode: 503});
-
-        assert.calledWith(v.req, {method: 'PUT', timeout: 12345});
-
-        v.req.reset();
-
-        const ans = sut.request({method: 'PUT', timeout: 12345}, {serverError: 'return'});
-
-        assert.equals(ans.statusCode, 503);
-        assert.equals(ans.message, 'Connection Refused');
-      });
-
-      test("general error", ()=>{
-        v.req.yields(new Error('foo'));
-        assert.exception(()=>{sut.request({method: 'HEAD'})}, {message: 'foo [500]', statusCode: 500});
-      });
-
-      test("success", ()=>{
-        const response = {headers: 'foo', statusCode: 200}, body = 'the body';
-        v.req.yields(null, response, body);
-
-        assert.equals(sut.request({method: 'HEAD'}), {statusCode: 200, response, body});
-      });
-
-      test("callback", ()=>{
-        const response = {headers: 'foo', statusCode: 200}, body = 'the body';
-        const callback = stub();
-
-        assert.equals(sut.request({method: 'HEAD'}, callback), undefined);
-
-        assert.calledWith(v.req, {method: 'HEAD', timeout: 20000}, callback);
-
-        refute.called(callback);
-      });
-
-      test("return 404, else throw", ()=>{
-        const response = {headers: 'foo', statusCode: 404}, body = 'the body';
-        v.req.yields(null, response, body);
-
-        const throw404 = {404: 'return', clientError: 'throw'};
-
-        assert.equals(sut.request({method: 'HEAD'}), {statusCode: 404, response, body});
-        assert.equals(sut.request({method: 'HEAD'}, throw404),
-                      {statusCode: 404, response, body});
-
-        response.statusCode = 409;
-        assert.equals(sut.request({method: 'HEAD'}), {statusCode: 409, response, body});
-        assert.exception(()=>{
-          sut.request({method: 'HEAD'}, throw404);
-        }, {statusCode: 409});
-      });
-
-      test("kill fiber", ()=>{
-        const handle = {abort: stub()};
-        v.req.returns(handle);
-        let fib, ans;
-        koru.runFiber(()=>{
-          fib = util.Fiber.current;
-          ans = sut.request({method: 'HEAD'});
-        });
-
-        fib.throwInto('testing 123');
-
-        assert.equals(ans, {interrupt: 'testing 123'});
-        assert.called(handle.abort);
-      });
-    });
-
-    group("expBackoff", ()=>{
-      beforeEach(()=>{
+  TH.testCase(module, ({beforeEach, afterEach, group, test}) => {
+    group('expBackoff', () => {
+      beforeEach(() => {
         let timerHandle = 100;
-        stub(koru, 'setTimeout').invokes(call => ++timerHandle);
+        stub(koru, 'setTimeout').invokes((call) => ++timerHandle);
         stub(koru, 'clearTimeout');
       });
 
-      test("success", ()=>{
+      test('success', () => {
         const config = {onSuccess: stub(), onFailure: stub()};
-        sut.expBackoff(()=>{
+        sut.expBackoff(() => {
           return {statusCode: 200};
         }, config);
-
 
         refute.called(config.onSuccess);
 
@@ -168,7 +30,6 @@ isServer && define((require, exports, module)=>{
         assert.equals(config.timer, 101);
         assert.equals(config.retryCount, 0);
 
-
         koru.setTimeout.yieldAndReset();
 
         refute.called(config.onFailure);
@@ -177,27 +38,29 @@ isServer && define((require, exports, module)=>{
         refute.called(koru.clearTimeout);
       });
 
-      test("failure", ()=>{
+      test('failure', () => {
+        let error;
         const config = {onSuccess: stub(), onFailure: stub()};
-        sut.expBackoff(()=>{
-          throw v.error = new sut.HttpError({statusCode: 400});
+        sut.expBackoff(() => {
+          throw error = new sut.HttpError({statusCode: 400});
         }, config);
 
         refute.called(config.onFailure);
 
         koru.setTimeout.yieldAndReset();
 
-        assert.calledWith(config.onFailure, v.error);
+        assert.calledWith(config.onFailure, error);
         refute.called(config.onSuccess);
 
         refute.called(koru.setTimeout);
         refute.called(koru.clearTimeout);
       });
 
-      test("retry", ()=>{
+      test('retry', () => {
+        let error;
         const config = {onSuccess: stub(), onFailure: stub()};
-        sut.expBackoff(()=>{
-          throw v.error = new sut.HttpError({statusCode: 500});
+        sut.expBackoff(() => {
+          throw error = new sut.HttpError({statusCode: 500});
         }, config);
 
         assert.same(config.retryCount, 0);
@@ -214,17 +77,18 @@ isServer && define((require, exports, module)=>{
 
         config.retryCount = 7;
 
-        config.isRetry = function (ex) {v.ex = ex; v.config = this; return true};
+        let exResult, configResult;
+        config.isRetry = function (ex) {exResult = ex; configResult = this; return true}
         koru.setTimeout.yieldAndReset();
 
-        assert.equals(v.config, config);
-        assert.equals(v.ex.statusCode, 500);
+        assert.equals(configResult, config);
+        assert.equals(exResult.statusCode, 500);
 
-        assert.calledWith(koru.setTimeout, match.func, match.between(3840*1000, (3840+10)*1000));
+        assert.calledWith(koru.setTimeout, match.func, match.between(3840*1000, (3840+10) * 1000));
         koru.setTimeout.yieldAndReset();
-        assert.calledWith(koru.setTimeout, match.func, match.between(90*60*1000, (90*60+10)*1000));
+        assert.calledWith(koru.setTimeout, match.func, match.between(90*60*1000, (90*60 + 10) * 1000));
         koru.setTimeout.yieldAndReset();
-        assert.calledWith(koru.setTimeout, match.func, match.between(90*60*1000, (90*60+10)*1000));
+        assert.calledWith(koru.setTimeout, match.func, match.between(90*60*1000, (90*60 + 10) * 1000));
 
         config.isRetry = () => false;
         koru.setTimeout.yieldAndReset();
@@ -233,62 +97,62 @@ isServer && define((require, exports, module)=>{
       });
     });
 
-    group("body", ()=>{
-      test("getBody json", ()=>{
+    group('body', () => {
+      test('getBody json', () => {
         const rawBody = {myBody: 'json🐧'}; // 🐧 to test default encoding
 
         assert.equals(sut.readBody(new HttpHelper.RequestStub({
           url: '/rest/2/sch00/foo/123/456?bar=abc&baz=123',
-          method: "GET",
+          method: 'GET',
           headers: {
-            'content-type': "application/json;charset=UTF-8"
+            'content-type': 'application/json;charset=UTF-8',
           },
         }, rawBody)), rawBody);
 
         assert.equals(sut.readBody(new HttpHelper.RequestStub({
           url: '/rest/2/sch00/foo/123/456?bar=abc&baz=123',
-          method: "GET",
+          method: 'GET',
           headers: {
-            'content-type': "text/html;charset=UTF-8"
+            'content-type': 'text/html;charset=UTF-8',
           },
         }, rawBody), {asJson: true}), rawBody);
 
         refute.equals(sut.readBody(new HttpHelper.RequestStub({
           url: '/rest/2/sch00/foo/123/456?bar=abc&baz=123',
-          method: "GET",
+          method: 'GET',
           headers: {
-            'content-type': "application/json;charset=UTF-8"
+            'content-type': 'application/json;charset=UTF-8',
           },
         }, rawBody), {encoding: 'binary'}), rawBody);
 
         assert.equals(sut.readBody(new HttpHelper.RequestStub({
           url: '/rest/2/sch00/foo/123/456?bar=abc&baz=123',
-          method: "GET",
+          method: 'GET',
           headers: {
-            'content-type': "text;charset=UTF-8"
+            'content-type': 'text;charset=UTF-8',
           },
         }, rawBody)), JSON.stringify(rawBody));
 
-        assert.exception(()=>{
+        assert.exception(() => {
           sut.readBody(new HttpHelper.RequestStub({
             url: '/rest/2/sch00/foo/123/456?bar=abc&baz=123',
-            method: "GET",
+            method: 'GET',
             headers: {
-              'content-type': "application/json;charset=UTF-8"
+              'content-type': 'application/json;charset=UTF-8',
             },
-          }, "junk"));
+          }, 'junk'));
         }, {error: 400, reason: 'request body must be valid JSON'});
       });
 
-      test("getBody limit", ()=>{
+      test('getBody limit', () => {
         const rawBody = {myBody: 'json'};
 
-        assert.exception(()=>{
+        assert.exception(() => {
           sut.readBody(new HttpHelper.RequestStub({
             url: '/rest/2/sch00/foo/123/456?bar=abc&baz=123',
-            method: "GET",
+            method: 'GET',
             headers: {
-              'content-type': "application/json;charset=UTF-8"
+              'content-type': 'application/json;charset=UTF-8',
             },
           }, rawBody), {limit: 4});
         }, {error: 413, reason: 'request body too large'});
@@ -296,9 +160,9 @@ isServer && define((require, exports, module)=>{
 
       class MyReq extends HttpHelper.RequestStub {
         _read() {
-          if (this._input === '')
+          if (this._input === '') {
             this.push(null);
-          else {
+          } else {
             const buf = this._input;
             this._input = '';
             this.push(buf);
@@ -306,22 +170,23 @@ isServer && define((require, exports, module)=>{
         }
       }
 
-      test("gzip", ()=>{
+      test('gzip', () => {
         const exp = {myBody: 'json'};
         const req = new MyReq({
           url: '/rest/2/sch00/foo/123/456?bar=abc&baz=123',
-          method: "GET",
+          method: 'GET',
           headers: {
-            'content-type': "application/json;charset=UTF-8",
-            'content-encoding': "gzip",
+            'content-type': 'application/json;charset=UTF-8',
+            'content-encoding': 'gzip',
           },
         }, zlib.gzipSync(JSON.stringify(exp)));
         assert.equals(sut.readBody(req), exp);
       });
     });
 
-    group("renderContent", ()=>{
-      test("render json", ()=>{
+    group('renderContent', () => {
+      test('render json', () => {
+        let v = {};
         const res = HttpHelper.makeResponse(v);
         sut.renderContent(res, {data: {foo: 123}});
         assert.equals(v.output, [JSON.stringify({foo: 123})]);
@@ -330,7 +195,8 @@ isServer && define((require, exports, module)=>{
         assert.isTrue(v.ended);
       });
 
-      test("render none", ()=>{
+      test('render none', () => {
+        let v = {};
         const res = HttpHelper.makeResponse(v);
         sut.renderContent(res, {});
         assert.equals(v.output, ['']);
@@ -339,11 +205,12 @@ isServer && define((require, exports, module)=>{
         assert.isTrue(v.ended);
       });
 
-      test("render custom", ()=>{
+      test('render custom', () => {
+        let v = {};
         const res = HttpHelper.makeResponse(v);
         sut.renderContent(res, {
           contentType: 'custom',
-          prefix: "my prefix", data: "the data", endcoding: 'binary',
+          prefix: 'my prefix', data: 'the data', endcoding: 'binary',
         });
         assert.equals(v.output, ['my prefix', 'the data']);
         assert.calledWith(res.writeHead, 200, {
--- app/koru/http-util.js
+++ app/koru/http-util.js
@@ -1,18 +1,17 @@
-define((require)=>{
+define((require) => {
   'use strict';
   const koru            = require('koru');
   const util            = require('koru/util');
   const stream          = requirejs.nodeRequire('stream');
   const zlib            = requirejs.nodeRequire('zlib');
 
-  let request = requirejs.nodeRequire('request');
-
-  const DAY24 = 24*util.DAY;
+  const DAY24 = 24 * util.DAY;
 
   class HttpError extends Error {
     constructor({message='Bad Request', statusCode, response, body}={}) {
-      if (statusCode === undefined)
+      if (statusCode === undefined) {
         statusCode = response === undefined ? 400 : response.statusCode;
+      }
       super(`${message} [${statusCode}]`);
       this.statusCode = statusCode;
       this.response = response;
@@ -20,98 +19,30 @@ define((require)=>{
     }
   }
 
-  const THROW_NONE = {};
-
-  const THROW_5XX = {
-    serverError: 'throw',
-  };
-
-  const THROW_ERROR = {
-    serverError: 'throw',
-    clientError: 'throw',
-  };
-
-  const THROW_ERROR_NOT_404 = Object.assign({404: 'return'}, THROW_ERROR);
-
-
   const HttpUtil = {
     HttpError,
-
-    THROW_NONE,
-    THROW_ERROR,
-    THROW_5XX,
-    THROW_ERROR_NOT_404,
-
-    request: (options, action)=>{
-      if (options.timeout === undefined)
-        options.timeout = 20*1000;
-
-      if (typeof action === 'function') {
-        return request(options, action);
-      }
-
-      const future = new util.Future;
-      let reqHandle;
-      reqHandle = request(options, (error, response, body)=>{
-        reqHandle = undefined;
-        future.return(error != null ? {
-          message: error.message,
-          statusCode: error.code === 'ETIMEDOUT'
-            ? 504 : (error.errno === 'ECONNREFUSED' ? 503 : 500)
-        } : {statusCode: response.statusCode, response, body});
-      });
-
-      let result;
-      try {
-        result = future.wait();
-      } catch(ex) {
-        if (reqHandle !== undefined)
-          reqHandle.abort();
-
-        if (ex instanceof Error) throw ex;
-        if (ex.constructor === Object)
-          return ex;
-        return {interrupt: ex};
-      }
-      const {statusCode} = result;
-      if (statusCode > 299) {
-        const actionOpts = action === undefined ? THROW_5XX : action;
-        let actionCmd = undefined;
-        if (statusCode > 399) {
-          if (statusCode > 499) {
-            actionCmd = actionOpts.serverError;
-          } else {
-            actionCmd = actionOpts[''+statusCode] || actionOpts.clientError;
-          }
-        }
-
-        if (actionCmd === 'throw')
-          throw new HttpError(result);
-      }
-      return result;
-    },
 
     expBackoff(func, config={}) {
       config.retryCount = 0;
       if (config.minDelay == null) config.minDelay = 30*1000;
-      if (config.maxDelay == null)
+      if (config.maxDelay == null) {
         config.maxDelay = 90*60*1000;
-      else if (config.maxDelay > DAY24)
+      } else if (config.maxDelay > DAY24) {
         throw new Error('config.maxDelay to big');
+      }
       if (config.variance == null) config.variance = 10*1000;
-      const wrapper = ()=>{
+      const wrapper = () => {
         config.timer = 0;
         try {
           const ans = func();
-        } catch(ex) {
+        } catch (ex) {
           if (config.isRetry === undefined ? ex.statusCode >= 500 : config.isRetry(ex)) {
             config.timer = koru.setTimeout(
               wrapper,
               Math.min(
                 config.maxDelay,
-                (2**config.retryCount++)*config.minDelay + Math.floor(Math.random()*config.variance))
+                (2 ** config.retryCount++) * config.minDelay + Math.floor(Math.random() * config.variance)),
             );
-
           } else if (config.onFailure === undefined) {
             throw ex;
           } else {
@@ -122,8 +53,8 @@ define((require)=>{
       config.timer = koru.setTimeout(wrapper, 0);
     },
 
-    pipeBody: (req, {limit=1000*1000})=>{
-      const input = /gzip|deflate/i.test(req.headers['content-encoding']||'')
+    pipeBody: (req, {limit=1000*1000}) => {
+      const input = /gzip|deflate/i.test(req.headers['content-encoding'] || '')
             ? req.pipe(zlib.createUnzip())
             : req;
 
@@ -131,48 +62,50 @@ define((require)=>{
 
       return input.pipe(new stream.Transform({
         transform(chunk, encoding, callback) {
-          if (size += chunk.length > limit)
+          if (size += chunk.length > limit) {
             callback(new koru.Error(413, 'request body too large'));
-          else
+          } else {
             callback(null, chunk);
-        }
+          }
+        },
       }));
     },
 
     readBody: (req, {
-      encoding="utf8",
+      encoding='utf8',
       limit,
       asJson=/\bjson\b/.test(req.headers['content-type']),
-    }={})=>{
-      const future = new util.Future;
+    }={}) => {
+      const future = new util.Future();
       {
         let string = '';
         const output = HttpUtil.pipeBody(req, {limit});
 
-        output.on('data', data =>{
+        output.on('data', (data) => {
           string += data.toString(encoding);
         });
-        output.on('error', err => {
-          if (err instanceof koru.Error)
+        output.on('error', (err) => {
+          if (err instanceof koru.Error) {
             future.throw([err.error, err.reason]);
-          else
-            future.throw([400, err+' '+util.inspect({code: err.code, errno: err.errno})]);
+          } else {
+            future.throw([400, err + ' ' + util.inspect({code: err.code, errno: err.errno})]);
+          }
         });
-        output.on('end', data =>{
+        output.on('end', (data) => {
           future.return(string);
         });
       }
       let data;
       let body;
       try {
-        body = future.wait() || "{}";
-      } catch(ex) {
+        body = future.wait() || '{}';
+      } catch (ex) {
         throw new koru.Error(...ex);
       }
       if (asJson) {
         try {
           data = JSON.parse(body);
-        } catch(ex) {
+        } catch (ex) {
           throw new koru.Error(400, 'request body must be valid JSON');
         }
 
@@ -197,7 +130,7 @@ define((require)=>{
         }
       }
       const header = {
-        'Content-Length': (prefix === undefined ? 0 : prefix.length)+Buffer.byteLength(data, encoding),
+        'Content-Length': (prefix === undefined ? 0 : prefix.length) + Buffer.byteLength(data, encoding),
         'Content-Type': `${contentType}; charset=${encoding}`,
       };
       if (eTag !== undefined) header.ETag = `W/"${eTag}"`;
@@ -208,8 +141,10 @@ define((require)=>{
   };
 
   if (isTest) HttpUtil[isTest] = {
-    get request() {return request},
-    set request(v) {request = v},
+    get https() {return https},
+    set https(v) {https = v},
+    get http() {return http},
+    set http(v) {http = v},
   };
 
   return HttpUtil;
