de06f2fb1da7d4bcc7caf6b54dff9f93e5576eab
Geoff Jacobsen <geoffjacobsen@gmail.com>
2022-02-18 19:34:48 +1300

    fix: bootstrap to be async

--- app/koru/migrate/bootstrap.js
+++ app/koru/migrate/bootstrap.js
@@ -1,21 +1,21 @@
-define((require, exports, module)=>{
+define((require, exports, module) => {
   'use strict';
   const koru            = require('koru');
   const Migration       = require('koru/migrate/migration');
   const {defaultDb}     = require('koru/pg/driver');
-
+  const fsp             = requirejs.nodeRequire('fs/promises');
   const path            = requirejs.nodeRequire('path');
-  const fs              = requirejs.nodeRequire('fs');
 
-  return ()=>{
+  return async () => {
     const migrateDir = path.resolve(koru.appDir, '../db/migrate');
     try {
-      fs.accessSync(migrateDir);
-    } catch(ex) {
-      if (ex.code !== 'ENOENT')
+      await fsp.access(migrateDir);
+    } catch (ex) {
+      if (ex.code !== 'ENOENT') {
         throw ex;
+      }
       return;
     }
-    new Migration(defaultDb).migrateTo(migrateDir, '~');
+    await new Migration(defaultDb).migrateTo(migrateDir, '~');
   };
 });
