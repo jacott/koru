a2a3371bb3bdf2c0e84cdeb5e00a382307906b6d
Geoff Jacobsen <geoffjacobsen@gmail.com>
2025-08-25 10:57:42 +1200

    style: reformat code

--- app/koru/test/client.js
+++ app/koru/test/client.js
@@ -1,6 +1,5 @@
-define((require, exports, module)=>{
+define((require, exports, module) => {
   'use strict';
-  const SessionBase = new (require('koru/session/base').constructor)('test');
   const koru            = require('koru/client'); // load client so we can override koru.logger
   const localStorage    = require('koru/local-storage');
   const sessState       = require('koru/session/state').constructor();
@@ -8,37 +7,41 @@ define((require, exports, module)=>{
   const Intercept       = require('./intercept');
   const Test            = require('./main');
 
-  const Module = module.constructor;
+  const SessionBase = new (require('koru/session/base').constructor)('test');
   const session = require('koru/session/main-client')(SessionBase, sessState);
+
+  const Module = module.constructor;
 
   if (koru.unregisterServiceWorker()) return;
 
-  koru.logger = (type, ...args)=>{
+  koru.logger = (type, ...args) => {
     if (type === 'E') {
       console.error(...args);
       session.send('E', args.join(' '));
     } else {
       console.log(...args);
-      session.send("L", type+ "> " + (
-        type === 'D' ? util.inspect(args, 7) :
-          args.join(' ')));
+      session.send('L', type + '> ' + (
+        type === 'D'
+          ? util.inspect(args, 7)
+          : args.join(' ')));
     }
   };
 
   Test.session = session;
 
-  Test.testHandle = (cmd, msg='')=>{session.send('T', cmd+msg)};
+  Test.testHandle = (cmd, msg='') => {session.send('T', cmd + msg)};
 
-  Test.logHandle = (type, msg)=>{
-    if (type === 'ERROR')
+  Test.logHandle = (type, msg) => {
+    if (type === 'ERROR') {
       session.send('E', msg);
-    else
+    } else {
       session.send('L', type + ': ' + msg);
+    }
   };
 
   let ls;
 
-  session.provide('T', data => {Test.run(data[0], data[1])});
+  session.provide('T', (data) => {Test.run(data[0], data[1])});
 
   const setItem = localStorage.setItem;
   const getItem = localStorage.getItem;
@@ -47,28 +50,28 @@ define((require, exports, module)=>{
 
   module.onUnload(() => {requirejs.onError = null});
 
-  localStorage._resetValue = ()=>Object.create(null);
+  localStorage._resetValue = () => Object.create(null);
   Test.Core.onStart(() => {
     localStorage.setItem = (key, value) => {
       const oldValue = ls[key];
       ls[key] = value;
     };
 
-    localStorage.getItem = key => {
+    localStorage.getItem = (key) => {
       const ans = ls[key];
       return ans === undefined ? null : ans;
     };
-    localStorage.removeItem = key => {
+    localStorage.removeItem = (key) => {
       delete ls[key];
     };
-    localStorage.clear = ()=>{
+    localStorage.clear = () => {
       ls = {};
     };
   });
 
-  Test.Core.onTestStart(()=>{ls = localStorage._resetValue()});
+  Test.Core.onTestStart(() => {ls = localStorage._resetValue()});
 
-  Test.Core.onEnd(()=>{
+  Test.Core.onEnd(() => {
     ls = null;
     localStorage.setItem = setItem;
     localStorage.getItem = getItem;
