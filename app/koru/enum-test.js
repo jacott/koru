0e0eaa74b0fe8308b4ca4946dda4187a3fce1ef0
Geoff Jacobsen <geoffjacobsen@gmail.com>
2025-12-19 08:18:56 +1300

    add: Enum explict numbers

--- app/koru/enum-test.js
+++ app/koru/enum-test.js
@@ -8,34 +8,37 @@ define((require, exports, module) => {
 
   TH.testCase(module, ({before, after, beforeEach, afterEach, group, test}) => {
     test('calling', () => {
-      const MyEnum = Enum(['one', 'two', 'three']);
+      const MyEnum = Enum(['one', 'two', 'three:6', 'four']);
       assert.same(MyEnum[0], 'one');
       assert.same(MyEnum[1], 'two');
-      assert.same(MyEnum[2], 'three');
+      assert.same(MyEnum[6], 'three');
+      assert.same(MyEnum[7], 'four');
       assert.same(MyEnum.one, 0);
       assert.same(MyEnum.two, 1);
-      assert.same(MyEnum.three, 2);
+      assert.same(MyEnum.three, 6);
+      assert.same(MyEnum.four, 7);
       assert.same(MyEnum.MIN, 0);
-      assert.same(MyEnum.MAX, 2);
+      assert.same(MyEnum.MAX, 7);
 
-      assert.equals(Enum.asList(MyEnum), 'one two three'.split(' '));
+      assert.equals(Enum.asList(MyEnum), 'one two three four'.split(' '));
       assert.equals(Enum.asMenuList(MyEnum, (n) => util.capitalize(n)), [
         {_id: 0, name: 'One'},
         {_id: 1, name: 'Two'},
-        {_id: 2, name: 'Three'},
+        {_id: 6, name: 'Three'},
+        {_id: 7, name: 'Four'},
       ]);
     });
 
     test('match', () => {
-      const MyEnum = Enum(['one', 'two', 'three']);
+      const MyEnum = Enum(['one', 'two:20', 'three']);
       const matcher = Enum.match(MyEnum);
 
-      assert.isFalse(matcher.test(3));
+      assert.isFalse(matcher.test(2));
+      assert.isFalse(matcher.test(22));
       assert.isFalse(matcher.test(-1));
       assert.isFalse(matcher.test('0'));
-      assert.isTrue(matcher.test(0));
-      assert.isTrue(matcher.test(1));
-      assert.isTrue(matcher.test(2));
+      assert.isTrue(matcher.test(20));
+      assert.isTrue(matcher.test(21));
     });
   });
 });
--- app/koru/enum.js
+++ app/koru/enum.js
@@ -7,14 +7,28 @@ define((require, exports, module) => {
 
   const Enum = (list) => {
     const en = Object.create(null);
-    list.forEach((n, i) => {
+    let i = 0;
+    for (const l of list) {
+      const [n, ni] = l.split(':');
+      if (ni !== undefined) {
+        i = +ni;
+        if (isNaN(i)) {
+          throw new Error(`invalid number at in Enum list at ${l} for ${list.join(', ')}`);
+        }
+      }
+
+      if (en[i] !== undefined) {
+        throw new Error(`entry already taken for ${n}:${i} for ${list.join(', ')}`);
+      }
+
       en[n] = i;
       en[i] = n;
-    });
+      ++i;
+    }
     en.MIN = 0;
-    en.MAX = list.length - 1;
+    en.MAX = i - 1;
     en[koru.__INTERCEPT$__] = Object.prototype[koru.__INTERCEPT$__];
-    en[match$] = match((value) => match.integer.test(value) && value >= en.MIN && value <= en.MAX);
+    en[match$] = match((value) => match.integer.test(value) && en[value] !== undefined);
     Object.freeze(en);
     return en;
   };
@@ -22,7 +36,9 @@ define((require, exports, module) => {
   Enum.asList = (enumType) => {
     const list = [];
     for (let i = enumType.MIN; i <= enumType.MAX; ++i) {
-      list.push(enumType[i]);
+      if (enumType[i] !== undefined) {
+        list.push(enumType[i]);
+      }
     }
     return list;
   };
@@ -32,7 +48,9 @@ define((require, exports, module) => {
   Enum.asMenuList = (enumType, mapper=ident) => {
     const list = [];
     for (let i = enumType.MIN; i <= enumType.MAX; ++i) {
-      list.push({_id: i, name: mapper(enumType[i])});
+      if (enumType[i] !== undefined) {
+        list.push({_id: i, name: mapper(enumType[i])});
+      }
     }
     return list;
   };
