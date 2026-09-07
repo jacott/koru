ef4e781c7052596bd7af234dd0ace317ae85a0ea
Geoff Jacobsen <geoffjacobsen@gmail.com>
2026-09-07 17:51:48 +1200

    style: format code

--- app/koru/pubsub/model-match.js
+++ app/koru/pubsub/model-match.js
@@ -11,8 +11,8 @@ define((require) => {
 
     has(doc) {
       const mm = this[models$][doc.constructor.modelName];
-      if (mm === void 0) return void 0;
-      let result = void 0;
+      if (mm === undefined) return;
+      let result = undefined;
       for (const comparator of mm) {
         const ans = comparator(doc);
         if (ans === true) return true;
@@ -26,7 +26,7 @@ define((require) => {
       modelName = typeof modelName === 'string' ? modelName : modelName.modelName;
 
       const models = this[models$];
-      const matchFuncs = models[modelName] || (models[modelName] = new DLinkedList());
+      const matchFuncs = models[modelName] ??= new DLinkedList();
       const handle = matchFuncs.add(comparator);
       handle.modelName = modelName;
       return handle;
@@ -35,10 +35,7 @@ define((require) => {
     _clear() {
       const models = this[models$];
       for (const modelName in models) {
-        const matchFuncs = models[modelName];
-        if (matchFuncs !== void 0) {
-          matchFuncs.clear();
-        }
+        models[modelName]?.clear();
       }
     }
   }
